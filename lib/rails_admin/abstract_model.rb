19c51e4c40fc4cea47b0c1036283e2f51b91b3fe
Erik Michaels-Ober <sferik@gmail.com>
2010-08-01 11:49:22 -0700

    add libraries from merb-admin

---
+++ lib/rails_admin/abstract_model.rb
@@ -0,0 +1,59 @@
+require 'rails_admin/generic_support'
+
+module MerbAdmin
+  class AbstractModel
+    # Returns all models for a given Merb app
+    def self.all
+      @models = []
+      Dir.glob(Rails.root.join("app/models/**/*.rb")).each do |filename|
+        File.read(filename).scan(/class ([\w\d_\-:]+)/).flatten.each do |model_name|
+          add_model(model_name)
+        end
+      end
+      @models.sort!{|x, y| x.model.to_s <=> y.model.to_s}
+    end
+
+    def self.add_model(model_name)
+      model = lookup(model_name)
+      @models << new(model) if model
+    end
+
+    # Given a string +model_name+, finds the corresponding model class
+    def self.lookup(model_name)
+      begin
+        # TODO: Should probably require the right part of ActiveSupport for this
+        model = model_name.constantize
+      rescue NameError
+        raise "MerbAdmin could not find model #{model_name}"
+      end
+
+      if superclasses(model).include?(ActiveRecord::Base)
+        model
+      else
+        nil
+      end
+    end
+
+    attr_accessor :model
+
+    def initialize(model)
+      model = self.class.lookup(model.to_s.camelize) unless model.is_a?(Class)
+      @model = model
+      self.extend(GenericSupport)
+      require 'rails_admin/active_record_support'
+      self.extend(ActiverecordSupport)
+    end
+
+    private
+
+    def self.superclasses(klass)
+      superclasses = []
+      while klass
+        superclasses << klass.superclass if klass && klass.superclass
+        klass = klass.superclass
+      end
+      superclasses
+    end
+
+  end
+end
