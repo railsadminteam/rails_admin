14f878dbd9e19260372a63f4e2e8a252e4511cf0
Erik Michaels-Ober <sferik@gmail.com>
2010-08-01 11:45:13 -0700

    add generic admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,10 @@
 RailsAdmin::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match "/admin", :to => "rails_admin#index", :as => :rails_admin_dashboard
+  match "/admin/:model_name", :to => "rails_admin#list", :as => :rails_admin_list
+  match "/admin/:model_name/new", :to => "rails_admin#new", :as => :rails_admin_new
+  match "/admin/:model_name/create", :to => "rails_admin#create", :as => :rails_admin_create
+  match "/admin/:model_name/edit", :to => "rails_admin#edit", :as => :rails_admin_edit
+  match "/admin/:model_name/update", :to => "rails_admin#update", :as => :rails_admin_update
+  match "/admin/:model_name/delete", :to => "rails_admin#delete", :as => :rails_admin_delete
+  match "/admin/:model_name/destroy", :to => "rails_admin#destroy", :as => :rails_admin_destroy
 end
