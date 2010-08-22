e804a5fa0cb248bb8432391ac24fa9ece78a0f9e
hurrycane <bogdan.gaza@yahoo.com>
2010-08-22 17:44:13 +0300

    Added cancel option to delete view. Also added tests. Closes #36

--- spec/requests/rails_admin_spec.rb
+++ spec/requests/rails_admin_spec.rb
@@ -736,6 +736,25 @@ describe "RailsAdmin" do
     end
   end
 
+  describe "destroy" do
+    before(:each) do
+      @player = RailsAdmin::AbstractModel.new("Player").create(:team_id => rand(99999), :number => 1, :name => "Player 1")
+
+      get rails_admin_delete_path(:model_name => "player", :id => @player.id)
+
+      @req = click_button "Cancel"
+      @player = RailsAdmin::AbstractModel.new("Player").first
+    end
+
+    it "should be successful" do
+      @req.should be_successful
+    end
+
+    it "should destroy an object" do
+      @player.should_not be_nil
+    end
+  end
+  
   describe "destroy with missing object" do
     before(:each) do
       @req = visit(rails_admin_destroy_path(:model_name => "player", :id => 1), :delete)
