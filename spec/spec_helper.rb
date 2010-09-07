7b7114276108010e54888fcaae8649a4d2497e6f
hurrycane <bogdan.gaza@yahoo.com>
2010-09-07 22:57:55 +0200

    Renamed dummy application to dummy_app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../dummy_app/config/environment.rb",  __FILE__)
 require "rails/test_help"
 require "rspec/rails"
 
@@ -12,7 +12,7 @@ ActionMailer::Base.default_url_options[:host] = "test.com"
 Rails.backtrace_cleaner.remove_silencers!
 
 # Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
+ActiveRecord::Migrator.migrate File.expand_path("../dummy_app/db/migrate/", __FILE__)
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
