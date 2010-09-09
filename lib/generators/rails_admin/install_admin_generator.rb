d684b245ebce95ee0987b8496e0242f7e5957117
hurrycane <bogdan.gaza@yahoo.com>
2010-09-09 05:37:34 +0200

    Changed structure of install_admin generator and modified an issue making the migration have the same number

--- lib/generators/rails_admin/install_admin_generator.rb
+++ lib/generators/rails_admin/install_admin_generator.rb
@@ -12,46 +12,7 @@ Rails_admin works with devise. Checking for a current installation of devise!
       is_loaded = loaded_gems.reject{|t| t.name == "devise" ? false : true }.size == 1 ? true : false
       
       if is_loaded
-        #File.exists?
-        devise_path =  FileUtils.pwd + "/config/initializers/devise.rb"
-        
-        if File.exists?(devise_path)
-          # check if migrations exist
-          app_path = Rails.public_path.split("/")
-          app_path.delete_at(-1)
-          app_path = app_path.join("/")
-          ###
-          routes_path = app_path + "/config/routes.rb"
-          
-          content = ""
-          
-          File.readlines(routes_path).each{|line|
-            content += line
-          }
-          
-          unless content.index("devise_for").nil?
-            # there is a devise_for in routes => Do nothing
-            puts "Great! You have devise installed and setup!"
-          else
-            puts "Great you have devise installed, but not set up!
-Setting up devise for you!
-======================================================"
-            invoke 'devise', ['user']
-            invoke 'devise', ['admin']
-          end
-          
-        else
-          puts "Looks like you don't have devise install! We'll install it for you!"
-          puts "Installing devise!
-======================================================"
-          invoke 'devise:install'
-          
-          puts "Setting up devise for you!
-======================================================"
-          invoke 'devise', ['user']
-          invoke 'devise', ['admin']
-        end
-        
+        check_for_devise_models
       else
         puts "Please put gem 'devise' into your Gemfile"
       end
@@ -64,6 +25,56 @@ Setting up devise for you!
     end
     
     private
+    
+    def check_for_devise_models
+      #File.exists?
+      devise_path =  FileUtils.pwd + "/config/initializers/devise.rb"
+      
+      if File.exists?(devise_path)
+        parse_route_files
+      else
+        puts "Looks like you don't have devise install! We'll install it for you!"
+        invoke 'devise:install'
+        set_devise
+        
+      end
+    end
+    
+    def parse_route_files
+      # check if migrations exist
+      app_path = Rails.public_path.split("/")
+      app_path.delete_at(-1)
+      app_path = app_path.join("/")
+      ###
+      routes_path = app_path + "/config/routes.rb"
+      
+      content = ""
+      
+      File.readlines(routes_path).each{|line|
+        content += line
+      }
+      
+      unless content.index("devise_for").nil?
+        # there is a devise_for in routes => Do nothing
+        puts "Great! You have devise installed and setup!"
+      else
+        puts "Great you have devise installed, but not set up!"
+        set_devise
+        
+      end
+      
+    end
+    
+    
+    def set_devise
+      puts "Setting up devise for you!
+======================================================"
+      invoke 'devise', ['user']
+    end
+    
+    def magick_goes_here
+      puts "I can do magick!"
+    end
     
     def copy_locales_files
       print "Now copying locales files! "
@@ -94,9 +105,6 @@ Setting up devise for you!
       print "\n"
       
     end
-    
-
-  
   end
   
   
--- lib/generators/rails_admin/install_migrations_generator.rb
+++ lib/generators/rails_admin/install_migrations_generator.rb
@@ -9,7 +9,10 @@ module RailsAdmin
 
     def self.next_migration_number(dirname)
       if ActiveRecord::Base.timestamped_migrations
-        Time.now.utc.strftime("%Y%m%d%H%M%S")
+        migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        migration_number += 1
+        migration_number.to_s
+        
       else
         "%.3d" % (current_migration_number(dirname) + 1)
       end
