2b29babb256ddbf31d32d41ead319f6cfdddf908
Daniel Neighman <has.sox@gmail.com>
2010-09-22 08:53:36 +1000

    Moves devise from a runtime dependency to a development dependency

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'devise'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
