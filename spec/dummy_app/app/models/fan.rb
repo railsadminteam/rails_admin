a5406dd7710fc51f157b1a57d1ea4961b7694236
Petteri Kääpä <petteri@kaapa.fi>
2010-09-30 05:33:07 +0800

    Revert Fan model to it's original state

--- spec/dummy_app/app/models/fan.rb
+++ spec/dummy_app/app/models/fan.rb
@@ -1,11 +1,5 @@
 class Fan < ActiveRecord::Base
   validates_presence_of(:name)
+
   has_and_belongs_to_many :teams
-  
-  RailsAdmin.config self do
-    label "Fans and friends"
-    label_for_navigation do
-      "#{config.label} Tab"
-    end
-  end
 end
