071e720c0a5e02065b85f0d69e2588d3e461f1af
toby cabot <ccabot@mitre.org>
2010-11-04 09:31:28 -0400

    move history_output() from main_helper to application_helper
    It wasn't accessible to the history view that renders the history for
    the AJAX slider updates.

--- app/helpers/rails_admin/main_helper.rb
+++ app/helpers/rails_admin/main_helper.rb
@@ -2,15 +2,6 @@ require 'builder'
 
 module RailsAdmin
   module MainHelper
-
-    def history_output(t)
-      if not t.message.downcase.rindex("changed").nil?
-        return t.message.downcase + " for #{t.table.capitalize} ##{t.item}"
-      else
-        return t.message.downcase
-      end
-    end
-
     def get_indicator(percent)
       percent = 0 if percent.nil?
       return "" if percent.between?(0, 33)
