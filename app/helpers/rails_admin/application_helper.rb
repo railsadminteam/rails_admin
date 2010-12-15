4091e557cf8c5686b490e797fc9773f520eaf3b3
toby cabot <ccabot@mitre.org>
2010-12-15 16:26:30 -0500

    fix table header sort bullet images
    It looks like they never got changed when the image path had
    rails_admin added to it.

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -7,5 +7,10 @@ module RailsAdmin
         return t.message.downcase
       end
     end
+
+    def sort_arrow_image(up = true)
+      image_name = up ? "bullet_arrow_up.png" : "bullet_arrow_down.png"
+      image_tag("rails_admin/" + image_name)
+    end
   end
 end
