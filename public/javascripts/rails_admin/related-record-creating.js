e372c78b7d6e88311a0af0e91ce02282b2643cf4
Sławosz Sławiński <slawosz@gmail.com>
2010-12-17 00:16:34 +0100

    improve new form rendering on facebox

--- public/javascripts/rails_admin/related-record-creating.js
+++ public/javascripts/rails_admin/related-record-creating.js
@@ -1,21 +1,27 @@
 jQuery(document).ready(function($) {
     function send_form(event) {
-        $('#facebox #new_record_form').bind("ajax:success", function(event, data, status, xhr){
-            $.facebox(data);
-        })
         $('#facebox #new_record_form').callRemote();
         event.preventDefault();
         return false;
     }
 
     
-    $('#show_associated_record_form').bind("ajax:success", function(event, data, status, xhr){
+    $('.show_associated_record_form').bind("ajax:success", function(event, data, status, xhr){
         $.facebox(data);
     })
 
-    $('#facebox #new_record_form').bind("ajax")
+    $('.selectField').bind('update_with_new_element', function(event, option_value, option_name) {
+        $(event.target).append('<option value="' + option_value + '">' + option_name + ' ' + option_value + '</option>' );
+    });
 
-    $('#facebox input#save_btn').live('click', send_form);
+    //$('#facebox #new_record_form').bind("ajax")
+
+    $('#facebox input.save_button').live('click', send_form);
+
+    $('#facebox input.cancel_button').live('click', function() {
+        $.facebox.close();
+        return false;
+    });
 
     
 });
