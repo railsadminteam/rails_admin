957e22f7c5ac3a82c0ad9c647f14329a90419b7e
Sławosz Sławiński <slawosz@cubiware.com>
2010-12-17 17:03:37 +0100

    add binary field

---
+++ lib/rails_admin/config/fields/types/binary.rb
@@ -0,0 +1,43 @@
+require 'builder'
+
+module RailsAdmin
+  module Config
+    module Fields
+      module Types
+        class Binary < RailsAdmin::Config::Fields::Base
+          # Register field type for the type loader
+          RailsAdmin::Config::Fields::Types::register(self)
+
+          register_instance_option(:column_css_class) do
+            "bool"
+          end
+
+          register_instance_option(:column_width) do
+            60
+          end
+
+          register_instance_option(:formatted_value) do
+            if value == true
+              Builder::XmlMarkup.new.img(:src => bindings[:view].image_path("bullet_black.png"), :alt => "True").html_safe
+            else
+              Builder::XmlMarkup.new.img(:src => bindings[:view].image_path("bullet_white.png"), :alt => "False").html_safe
+            end
+          end
+
+          # Accessor for field's help text displayed below input field.
+          register_instance_option(:help) do
+            ""
+          end
+
+          register_instance_option(:searchable?) do
+            false
+          end
+
+          register_instance_option(:sortable?) do
+            true
+          end
+        end
+      end
+    end
+  end
+end
