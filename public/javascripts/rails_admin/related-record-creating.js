20ed4d56d672ecad7c3c27fb55bbaf03e416f91d
Sławosz Sławiński <slawosz@gmail.com>
2010-12-18 19:12:14 +0100

    small css, dom, and javascript improvements

--- public/javascripts/rails_admin/related-record-creating.js
+++ public/javascripts/rails_admin/related-record-creating.js
@@ -1,26 +1,25 @@
 jQuery(document).ready(function($) {
     function send_form(event) {
-        $('#facebox #new_record_form').callRemote();
         event.preventDefault();
-        return false;
+        $('#facebox form').callRemote();
     }
 
     
-    $('.show_associated_record_form').bind("ajax:success", function(event, data, status, xhr){
+    $('.showAssociatedRecordForm').bind("ajax:success", function(event, data, status, xhr){
         $.facebox(data);
     })
 
-    $('.selectField').bind('update_with_new_element', function(event, option_value, option_name) {
+    $('.selectField').bind('updateWithNewElement', function(event, option_value, option_name) {
         $(event.target).append('<option value="' + option_value + '">' + option_name + '</option>' );
     });
 
     //$('#facebox #new_record_form').bind("ajax")
 
-    $('#facebox input.save_button').live('click', send_form);
+    $('#facebox input[name=_save]').live('click', send_form);
 
-    $('#facebox input.cancel_button').live('click', function() {
+    $('#facebox input[name=_continue]').live('click', function(event) {
+        event.preventDefault();
         $.facebox.close();
-        return false;
     });
 
     
