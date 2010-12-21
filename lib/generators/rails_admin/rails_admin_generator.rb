29ca47586f2728654b0aff0948176119d502f961
Petteri Kääpä <petteri.kaapa@gmail.com>
2010-12-21 17:36:44 +0200

    Document how to override Devise model in install generator.

--- lib/generators/rails_admin/rails_admin_generator.rb
+++ lib/generators/rails_admin/rails_admin_generator.rb
@@ -5,7 +5,18 @@ module RailsAdmin
 
     def print_instructions
       puts "RailsAdmin!
-Hello, to install rails_admin into your app you need to run rails g rails_admin:install_admin"
+
+Hello, to install rails_admin into your app you need to run:
+
+  rails g rails_admin:install_admin
+
+By default RailsAdmin works with Devise to provide authentication. If you use
+Devise, but want use another model than the default 'user' you can provide the
+custom model name as an argument:
+
+  rails g rails_admin:install_admin member
+
+"
     end
 
   end
