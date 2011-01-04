e1a11763a1d5b4af88b9f6a405bea0ffcf42f034
Erik Michaels-Ober <sferik@gmail.com>
2011-01-04 12:59:59 -0800

    Start each spec run with a fresh database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,8 @@ Rails.backtrace_cleaner.remove_silencers!
 
 include DatabaseHelpers
 # Run any available migration
-puts 'setting up database'
+puts 'Setting up database...'
+drop_all_tables
 migrate_database
 
 # Load support files
