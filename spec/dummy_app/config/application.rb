8dbda743a200d137aee18545491035170cf2280f
Jason Hoth Jr <jason_hoth_jr@his-service.net>
2011-01-10 22:35:58 -0800

    attempt to revert paperclip-related-bug: '0c3f0527bf6fe1983b3da2a31d6e49e548e9f144'

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'devise'
-require 'paperclip'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
