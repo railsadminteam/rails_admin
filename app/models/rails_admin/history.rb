e95063d5f98b6184239d360d7a6b516dde1eface
toby cabot <toby@caboteria.org>
2011-01-22 23:40:43 -0500

    rename histories table to rails_admin_histories
    The problem with "histories" is that it's an obvious enough name that
    it's likely to cause conflicts with existing tables.  We've already
    seen one issue filed because of this.
    
    This patch adds another migration to the existing generator to change
    the name of the histories table, and changes the History class to use
    that name.

--- app/models/rails_admin/history.rb
+++ app/models/rails_admin/history.rb
@@ -1,5 +1,6 @@
 module RailsAdmin
   class History < ActiveRecord::Base
+    set_table_name :rails_admin_histories
 
     IGNORED_ATTRS = Set[:id, :created_at, :created_on, :deleted_at, :updated_at, :updated_on, :deleted_on]
 
@@ -13,14 +14,14 @@ module RailsAdmin
         sql_in = (mstart + 1..12).to_a.join(", ")
         sql_in_two = (1..mstop).to_a.join(", ")
 
-        results = History.find_by_sql("select count(*) as number, year, month from histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
-        results_two = History.find_by_sql("select count(*) as number, year, month from histories where month IN (#{sql_in_two}) and year = #{ystop} group by year, month")
+        results = History.find_by_sql("select count(*) as number, year, month from rails_admin_histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
+        results_two = History.find_by_sql("select count(*) as number, year, month from rails_admin_histories where month IN (#{sql_in_two}) and year = #{ystop} group by year, month")
 
         results.concat(results_two)
       else
         sql_in =  (mstart + 1..mstop).to_a.join(", ")
 
-        results = History.find_by_sql("select count(*) as number, year, month from histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
+        results = History.find_by_sql("select count(*) as number, year, month from rails_admin_histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
       end
 
       results.each do |result|
--- lib/generators/rails_admin/install_migrations_generator.rb
+++ lib/generators/rails_admin/install_migrations_generator.rb
@@ -17,7 +17,9 @@ module RailsAdmin
     end
 
     def create_migration_file
-      migration_template 'migration.rb', 'db/migrate/create_histories_table.rb'
+      migration_template 'migration.rb', 'db/migrate/create_histories_table.rb' rescue p $!.message
+      sleep 1 # ensure scripts have different timestamps
+      migration_template 'rename.rb', 'db/migrate/rename_histories_to_rails_admin_histories.rb'
     end
   end
 end
