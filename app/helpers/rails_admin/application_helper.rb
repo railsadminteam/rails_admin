44ea562b7a489921931e943f4affaa19072e5e4f
Petteri Kääpä <petteri.kaapa@gmail.com>
2011-02-03 01:00:57 +0200

    Add helpers for localization of day and month names

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -8,6 +8,36 @@ module RailsAdmin
       end
     end
 
+    def abbr_day_names
+      I18n.t('date.abbr_day_names', :raise => true)
+    rescue I18n::ArgumentError
+      I18n.t('date.abbr_day_names', :locale => :en)
+    end
+
+    def abbr_month_names
+      names = I18n.t('date.abbr_month_names', :raise => true)
+    rescue I18n::ArgumentError
+      names = I18n.t('date.abbr_month_names', :locale => :en)
+      names[1..-1]
+    end
+
+    def date_format
+      I18n.t('date.formats.default', :default => I18n.t('date.formats.default', :locale => :en))
+    end
+
+    def day_names
+      I18n.t('date.day_names', :raise => true)
+    rescue I18n::ArgumentError
+      I18n.t('date.day_names', :locale => :en)
+    end
+
+    def month_names
+      names = I18n.t('date.month_names', :raise => true)
+    rescue I18n::ArgumentError
+      names = I18n.t('date.month_names', :locale => :en)
+      names[1..-1]
+    end
+
     # Given a page count and the current page, we generate a set of pagination
     # links.
     #
