eb4e8c8fa8be1393425aede35390dca7c136f857
Petteri Kääpä <petteri.kaapa@gmail.com>
2011-02-04 04:24:43 +0200

    Move jQuery no-conflict initialization to application.js

--- public/javascripts/rails_admin/application.js
+++ public/javascripts/rails_admin/application.js
@@ -1,3 +1,1 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
+var $j = jQuery.noConflict();
--- public/javascripts/rails_admin/jquery.no-conflict.js
+++
@@ -1,1 +0,0 @@
-var $j = jQuery.noConflict();
