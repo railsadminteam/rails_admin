bcf82bf28b52376de3792065fce94b3a8e42ac67
Petteri Kääpä <petteri.kaapa@gmail.com>
2011-02-15 22:57:18 +0200

    Have paperclip field type return nil if no file has been saved

--- lib/rails_admin/config/fields/types/paperclip_file.rb
+++ lib/rails_admin/config/fields/types/paperclip_file.rb
@@ -12,6 +12,14 @@ module RailsAdmin
           register_instance_option(:partial) do
             :form_paperclip_file
           end
+
+          def value
+            if bindings[:object].send(name).file?
+              bindings[:object].send(name).url
+            else
+              nil
+            end
+          end
         end
       end
     end
