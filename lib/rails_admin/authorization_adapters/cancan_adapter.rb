fc8748e212c60fa8f6f281a2dd6fba4d41cef0b9
Ryan Bates <ryan@railscasts.com>
2011-02-23 15:32:49 -0800

    adding ':access, :rails_admin' authorization check for each action

--- lib/rails_admin/authorization_adapters/cancan_adapter.rb
+++ lib/rails_admin/authorization_adapters/cancan_adapter.rb
@@ -6,16 +6,32 @@ module RailsAdmin
         @controller.extend ControllerExtension
       end
 
-      def authorize(action, model_class, model_object = nil)
-        @controller.current_ability.authorize!(action, model_object || model_class)
+      def authorize(action, model_class = nil, model_object = nil)
+        action = translate_action(action)
+        @controller.current_ability.authorize! :access, :rails_admin
+        @controller.current_ability.authorize!(action, model_object || model_class) if action
       end
 
       def query(model_class, action)
+        action = translate_action(action)
         model_class.accessible_by(@controller.current_ability, action)
       end
 
       def attributes_for(action, model_class)
+        action = translate_action(action)
         @controller.current_ability.attributes_for(action, model_class)
+      end
+
+      private
+
+      # Change the action into something that fits better with CanCan's conventions
+      def translate_action(action)
+        case action
+        when :index then nil # we don't want to do extra action authorization for dashboard
+        when :list then :index
+        when :delete then :destroy
+        else action
+        end
       end
 
       module ControllerExtension
