996473d009445d22a3a8dcf332527b7158a2ae28
Petteri Kääpä <petteri.kaapa@gmail.com>
2011-03-02 02:49:56 +0200

    Include a method to query object_label method names in Labelable

--- lib/rails_admin/config/labelable.rb
+++ lib/rails_admin/config/labelable.rb
@@ -5,8 +5,15 @@ module RailsAdmin
       # Try to find a user-friendly label for an object, falling back
       # to its class and ID.
       def self.object_label(object)
-        Config.label_methods.each {|l| label = (object.respond_to? l and object.send l) and return label}
-        "#{object.class.to_s} ##{object.try :id}"
+        if method = self.object_label_method(object)
+          object.send method
+        else
+          "#{object.class.to_s} ##{object.try :id}"
+        end
+      end
+
+      def self.object_label_method(object)
+        Config.label_methods.find {|method| object.respond_to? method }
       end
 
       def self.included(klass)
@@ -14,6 +21,7 @@ module RailsAdmin
           abstract_model.model.model_name.human(:default => abstract_model.model.model_name.titleize)
         end
         klass.register_instance_option(:object_label) {Labelable.object_label bindings[:object]}
+        klass.register_instance_option(:object_label_method) {Labelable.object_label_method bindings[:object]}
       end
     end
   end
