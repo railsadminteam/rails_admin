61b827d8764c3fcad588fc6b98e6a484cc6bbfc0
cschiewek <curtis.schiewek@gmail.com>
2011-03-21 09:58:25 -0400

    Changed the 'number' field in histories to record_count, as number is a reserved word in Oracle.

--- app/models/rails_admin/blank_history.rb
+++ app/models/rails_admin/blank_history.rb
@@ -2,14 +2,14 @@ module RailsAdmin
   class BlankHistory
     attr_accessor :number, :month, :year
     def initialize(month, year)
-      @number = 0
+      @record_count = 0
       @month = month
       @year = year
     end
 
     # Make BlankHistory look like History when it gets JSON-serialized.
     def to_hash(*a)
-      {"history" => {"number" => @number, "month" => @month, "year" => @year}}
+      {"history" => {"record_count" => @record_count, "month" => @month, "year" => @year}}
     end
 
   end
