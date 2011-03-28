b16dffb055fda61fa0f51924cfc3a6cdcd8d1b7f
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-03-28 15:07:57 -0700

    Patch handling of namespaced models like Cms::BasicPage. Fixes #352, http://goo.gl/H1mLV 1. Don't generate urls with :: in them, that's not legal (see http://www.faqs.org/rfcs/rfc2396.html Section 2.2, 2.3). Use ~ instead. This is still hacky. Ideally we should treat namespaces as proper URL components, e.g. /cms/basic_page/5/edit, but that would have been a bigger change. 2. Rails will automatically change model names containing "::" into "_" when generating input field names. A recent commit started using Rails's native form helpers to generate input field names which completely broke namespaced model support. This has been patched by gsub'ing "~" -> "_" in incoming parameters. This is a stop gap measure pending a wider resolution of the namespace issue. This fixes # 3. Added test support for namespaced models which was lacking and probably the reason why previous support regressed.

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -273,7 +273,7 @@ module RailsAdmin
     end
 
     def get_attributes
-      @attributes = params[@abstract_model.to_param.singularize] || {}
+      @attributes = params[@abstract_model.to_param.singularize.gsub('~','_')] || {}
       @attributes.each do |key, value|
         # Deserialize the attribute if attribute is serialized
         if @abstract_model.model.serialized_attributes.keys.include?(key) and value.is_a? String
