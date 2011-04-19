2aee85f40d5358bc41b14afb07f53ab245b54452
toby cabot <toby@caboteria.org>
2011-04-19 07:39:02 -0400

    work around a meta_where bug
    MetaWhere's "overload_operator!" method changes the semantics of
    Symbol's "=~" method which abstract_object uses during initialization.
    By calling Symbol.to_s we use String's "=~" method instead which
    seems to work.
    
    Closes github issue #374.

--- lib/rails_admin/abstract_object.rb
+++ lib/rails_admin/abstract_object.rb
@@ -1,6 +1,11 @@
  module RailsAdmin
   class AbstractObject
-    instance_methods.each { |m| undef_method m unless m =~ /(^__|^send$|^object_id$)/ }
+    # undef almost all of this class's methods so it will pass almost
+    # everything through to its delegate using method_missing (below).
+    instance_methods.each { |m| undef_method m unless m.to_s =~ /(^__|^send$|^object_id$)/ }
+    #                                                  ^^^^^
+    # the unnecessary "to_s" above is a workaround for meta_where, see
+    # https://github.com/sferik/rails_admin/issues/374
 
     attr_accessor :object
     attr_accessor :associations
