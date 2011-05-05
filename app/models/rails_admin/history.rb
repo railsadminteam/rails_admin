72acb26ba5a6b0dee11b8638e154b7b7d86c9472
Arthur Gunn <arthur@gunn.co.nz>
2011-05-06 01:18:30 +1200

    Backported fixes for history.rb

--- app/models/rails_admin/history.rb
+++ app/models/rails_admin/history.rb
@@ -1,36 +1,35 @@
 module RailsAdmin
   class History < ActiveRecord::Base
     set_table_name :rails_admin_histories
-
+ 
     IGNORED_ATTRS = Set[:id, :created_at, :created_on, :deleted_at, :updated_at, :updated_on, :deleted_on]
-
+ 
     scope :most_recent, lambda {|table|
       where("#{retrieve_connection.quote_column_name(:table)} = ?", table).order("updated_at")
     }
-
+ 
     def self.get_history_for_dates(mstart, mstop, ystart, ystop)
-      sql_in = ""
-      if mstart > mstop
-        sql_in = (mstart + 1..12).to_a.join(", ")
+      if mstart > mstop && mstart < 12
+        sql_in = ((mstart + 1)..12).to_a.join(", ")
         sql_in_two = (1..mstop).to_a.join(", ")
-
-        results = History.find_by_sql("select count(*) as number, year, month from rails_admin_histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
-        results_two = History.find_by_sql("select count(*) as number, year, month from rails_admin_histories where month IN (#{sql_in_two}) and year = #{ystop} group by year, month")
-
+ 
+        results = History.find_by_sql("select count(*) as record_count, year, month from rails_admin_histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
+        results_two = History.find_by_sql("select count(*) as record_count, year, month from rails_admin_histories where month IN (#{sql_in_two}) and year = #{ystop} group by year, month")
+ 
         results.concat(results_two)
       else
-        sql_in =  (mstart + 1..mstop).to_a.join(", ")
-
-        results = History.find_by_sql("select count(*) as number, year, month from rails_admin_histories where month IN (#{sql_in}) and year = #{ystart} group by year, month")
+        sql_in = ((mstart == 12 ? 1 : mstart + 1)..mstop).to_a.join(", ")
+ 
+        results = History.find_by_sql("select count(*) as record_count, year, month from rails_admin_histories where month IN (#{sql_in}) and year = #{ystop} group by year, month")
       end
-
+ 
       results.each do |result|
-        result.number = result.number.to_i
+        result.record_count = result.record_count.to_i
       end
-
+ 
       add_blank_results(results, mstart, ystart)
     end
-
+ 
     def self.add_blank_results(results, mstart, ystart)
       # fill in an array with BlankHistory
       blanks = Array.new(5) { |i| BlankHistory.new(((mstart+i) % 12)+1, ystart + ((mstart+i)/12)) }
