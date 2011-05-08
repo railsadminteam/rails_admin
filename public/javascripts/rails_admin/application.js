0354396be396320519dadba7a4cb7cd07f8aed8b
Arthur Gunn <arthur@gunn.co.nz>
2011-05-09 00:05:23 +1200

    Make "Select All" checkbox on list page work. Closes #7.

--- public/javascripts/rails_admin/application.js
+++ public/javascripts/rails_admin/application.js
@@ -4,4 +4,19 @@ if (typeof($j) === "undefined" && typeof(jQuery) !== "undefined") {
 
 $j(document).ready(function($){
   $(".ra-button").not(".ui-button").button({});
+  
+  // On the list page, the checkbox in th table's header toggles all the checkboxes underneath it.
+  $("table.table input.checkbox.toggle").click(function() {
+    var checked_status = $(this).is(":checked");
+    $("td.action.select input.checkbox[name='bulk_ids[]']").each(function() {
+      $(this).attr('checked', checked_status);
+      
+      if (checked_status) {
+        $(this).parent().addClass("checked");
+      } else {
+        $(this).parent().removeClass("checked");
+      }
+      
+    });
+  });
 });
