40b4a2fc118cc0d50f3a44d4aaccb74d8aba22ad
Benoit Bénézech <benoit.benezech@elapse.fr>
2011-05-11 12:41:38 +0200

    Typo. This shows I need tests for that feature. Will add it tmrw.

--- lib/rails_admin/config/fields/types/has_many_association.rb
+++ lib/rails_admin/config/fields/types/has_many_association.rb
@@ -25,7 +25,7 @@ module RailsAdmin
           end
           
           # orderable associated objects
-          register_instance_option(:orderable?) do
+          register_instance_option(:orderable) do
             false
           end
 
