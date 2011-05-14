6160225bc86ef0a8acf4ca19c82ba27a0cbe64ac
Peter Suschlik <peter@suschlik.de>
2011-05-14 15:13:58 +0200

    Revert wrong fixes

--- lib/generators/rails_admin/rails_admin_generator.rb
+++ lib/generators/rails_admin/rails_admin_generator.rb
@@ -8,7 +8,7 @@ module RailsAdmin
 
 Hello, to install rails_admin into your app you need to run:
 
-  rails g rails_admin:install_migrations
+  rake rails_admin:install
 
 By default RailsAdmin works with Devise to provide authentication. If you use
 Devise, but want use another model than the default 'user' you can provide the
