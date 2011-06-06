c1eeec233ef6f8e8eb3eabe8e10a40482795ce0b
Benoit Bénézech <benoit.benezech@gmail.com>
2011-06-06 15:07:25 +0200

    Shabby and doesn't work witgh rails 3.1

--- spec/dummy_app/app/models/league.rb
+++ spec/dummy_app/app/models/league.rb
@@ -2,7 +2,6 @@ class League < ActiveRecord::Base
   validates_presence_of(:name)
 
   has_many(:divisions)
-  has_many(:teams)
 
   def teams
   	Team.joins(:division).where("divisions.league_id" => id)
