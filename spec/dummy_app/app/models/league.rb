5c6c6719e3cb75a6371a1952841f4d270055a115
Benoit Bénézech <benoit.benezech@gmail.com>
2011-06-06 15:09:30 +0200

    Let's convert it to a regular HasMany :Through.

--- spec/dummy_app/app/models/league.rb
+++ spec/dummy_app/app/models/league.rb
@@ -1,12 +1,9 @@
 class League < ActiveRecord::Base
   validates_presence_of(:name)
 
-  has_many(:divisions)
+  has_many :divisions
+  has_many :teams, :through => :divisions
 
-  def teams
-  	Team.joins(:division).where("divisions.league_id" => id)
-  end
-  
   def custom_name
     "League '#{self.name}'"
   end
