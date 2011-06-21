31e7c93d3bde00005fb6d5112b3d5ea03ff70f3b
Petteri Kääpä <petteri.kaapa@gmail.com>
2011-06-21 19:43:43 +0300

    Deprecate Navigation.max_visible_tabs option as navigation is a vertical list now, fixes #478

--- lib/rails_admin/config/sections/navigation.rb
+++ lib/rails_admin/config/sections/navigation.rb
@@ -8,7 +8,11 @@ module RailsAdmin
       class Navigation < RailsAdmin::Config::Base
         # Defines the number of tabs to be renderer in the main navigation.
         # Rest of the links will be rendered to a drop down menu.
+        # NOTE: "max_visible_tabs" is deprecated
+        # FIXME: remove this after giving people an appropriate time
+        # to change their code.
         register_class_option(:max_visible_tabs) do
+          ActiveSupport::Deprecation.warn("Navigation.max_visible_tabs configuration option is deprecated", caller)
           5
         end
       end
