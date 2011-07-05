813763f341ee1c4c79f42bb2cabe1d5089741b33
Arthur Gunn <arthur@gunn.co.nz>
2011-07-05 15:40:50 +1200

    Attempt at fixing checkbox values for #492.

--- lib/rails_admin/config/fields/types/boolean.rb
+++ lib/rails_admin/config/fields/types/boolean.rb
@@ -26,7 +26,7 @@ module RailsAdmin
           register_instance_option(:html_attributes) do
             {
               :class => "#{css_class} #{has_errors? ? "errorField" : nil}",
-              :checked => (value == "1") ? true : false,
+              :checked => (value && value.to_s != "0"),
               :style => "width:#{column_width}px",
             }
           end
