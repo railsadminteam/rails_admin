041a3aabd5142f7baa67d4c0a4042f817af37a82
Amerson Lin & Mike Mazur <pair+amerson+mmazur@pivotallabs.com>
2011-07-11 10:10:18 +0800

    Converted spec to capybara

--- spec/requests/basic/show/rails_admin_basic_show_spec.rb
+++ spec/requests/basic/show/rails_admin_basic_show_spec.rb
@@ -5,33 +5,24 @@ describe "RailsAdmin Basic Show" do
   describe "show" do
     before(:each) do
       @player = Factory.create :player
-      get rails_admin_show_path(:model_name => "player", :id => @player.id)
-    end
-
-    it "should respond successfully" do
-      response.should be_successful
-    end
-
-    it "should load @abstract_model, @object" do
-      assigns(:abstract_model).should be
-      assigns(:object).should be
+      visit rails_admin_show_path(:model_name => "player", :id => @player.id)
     end
 
     it "should have History, Edit, Delete, Cancel buttons" do
-      response.body.should have_selector("a.button", :text => "History")
-      response.body.should have_selector("a.button", :text => "Edit")
-      response.body.should have_selector("a.button", :text => "Delete")
+      page.should have_selector("a.button", :text => "History")
+      page.should have_selector("a.button", :text => "Edit")
+      page.should have_selector("a.button", :text => "Delete")
     end
 
     it "should show 'Details'" do
-      response.body.should have_content("Details")
+      page.should have_content("Details")
     end
 
     it "should show attributes" do
-      response.body.should have_content("Name")
-      response.body.should have_content(@player.name)
-      response.body.should have_content("Number")
-      response.body.should have_content(@player.number)
+      page.should have_content("Name")
+      page.should have_content(@player.name)
+      page.should have_content("Number")
+      page.should have_content(@player.number)
     end
   end
 
@@ -40,15 +31,11 @@ describe "RailsAdmin Basic Show" do
       @player = Factory.create :player
       @team   = Factory.create :team
       @player.update_attribute(:team, @team)
-      get rails_admin_show_path(:model_name => "player", :id => @player.id)
-    end
-
-    it "should respond successfully" do
-      response.should be_successful
+      visit rails_admin_show_path(:model_name => "player", :id => @player.id)
     end
 
     it "should show associated objects" do
-      response.body.should have_selector("div.value", :text => "Team ##{@team.id}")
+      page.should have_selector("div.value", :text => "Team ##{@team.id}")
     end
   end
 
@@ -56,15 +43,11 @@ describe "RailsAdmin Basic Show" do
     before(:each) do
       @player = Factory.create :player
       @draft  = Factory.create :draft, :player => @player
-      get rails_admin_show_path(:model_name => "player", :id => @player.id)
-    end
-
-    it "should respond successfully" do
-      response.should be_successful
+      visit rails_admin_show_path(:model_name => "player", :id => @player.id)
     end
 
     it "should show associated objects" do
-      response.body.should have_selector("div.value", :text => "Draft ##{@draft.id}")
+      page.should have_selector("div.value", :text => "Draft ##{@draft.id}")
     end
   end
 
@@ -75,27 +58,13 @@ describe "RailsAdmin Basic Show" do
       @comment2 = Factory.create :comment, :commentable => @player
       @comment3 = Factory.create :comment, :commentable => Factory.create(:player)
 
-      get rails_admin_show_path(:model_name => "player", :id => @player.id)
-    end
-
-    it "should respond successfully" do
-      response.should be_successful
+      visit rails_admin_show_path(:model_name => "player", :id => @player.id)
     end
 
     it "should show associated objects" do
-      response.body.should have_selector("div.value", :text => "Comment ##{@comment1.id}")
-      response.body.should have_selector("div.value", :text => "Comment ##{@comment2.id}")
-      response.body.should_not have_selector("div.value", :text => "Comment ##{@comment3.id}")
-    end
-  end
-
-  describe "show with missing object" do
-    before(:each) do
-      get rails_admin_show_path(:model_name => "player", :id => 1)
-    end
-
-    it "should raise NotFound" do
-      response.status.should equal(404)
+      page.should have_selector("div.value", :text => "Comment ##{@comment1.id}")
+      page.should have_selector("div.value", :text => "Comment ##{@comment2.id}")
+      page.should_not have_selector("div.value", :text => "Comment ##{@comment3.id}")
     end
   end
 
@@ -103,15 +72,11 @@ describe "RailsAdmin Basic Show" do
     before(:each) do
       @player = Factory.create :player
       @comment = Factory.create :comment, :commentable => @player
-      get rails_admin_show_path(:model_name => "comment", :id => @comment.id)
-    end
-
-    it "should respond successfully" do
-      response.should be_successful
+      visit rails_admin_show_path(:model_name => "comment", :id => @comment.id)
     end
 
     it "should show associated object" do
-      response.body.should have_selector("div.value", :text => "Player ##{@player.id}")
+      page.should have_selector("div.value", :text => "Player ##{@player.id}")
     end
   end
 end
