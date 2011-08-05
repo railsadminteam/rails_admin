15c805ae1be6ab1464aef69f109b9ec2c7e69e19
Benoit Bénézech <benoit.benezech@gmail.com>
2011-08-05 15:39:12 +0200

    no need to wait for document to be ready: we're using live

--- app/assets/javascripts/rails_admin/application.js
+++ app/assets/javascripts/rails_admin/application.js
@@ -2,33 +2,29 @@ if (typeof($j) === "undefined" && typeof(jQuery) !== "undefined") {
   var $j = jQuery.noConflict();
 }
 
-$j(document).ready(function($){
-  // accordeon
-  $("#nav .more a").live('click', function() {
-    $(this).siblings('ul').toggle('slide');
+// accordeon
+$j("#nav .more a").live('click', function() {
+  $j(this).siblings('ul').toggle('slide');
+});
+
+$j("table.table tr.link").live('click', function(e) {
+  // trs and tds are things that we want to link to the edit page
+  // if the click's target is a button for instance, we don't want to move the user.
+  if ($j(e.target).is('tr') || $j(e.target).is('td') || $j(e.target).is('div.bar')) {
+    window.location.href = $j(this).attr("data-link");
+  };
+});
+
+// On the list page, the checkbox in th table's header toggles all the checkboxes underneath it.
+$j("table.table input.checkbox.toggle").live('click', function() {
+  var checked_status = $j(this).is(":checked");
+  $j("td.action.select input.checkbox[name='bulk_ids[]']").each(function() {
+    $j(this).attr('checked', checked_status);
+
+    if (checked_status) {
+      $j(this).parent().addClass("checked");
+    } else {
+      $j(this).parent().removeClass("checked");
+    }
   });
-  
-  $("table.table tr.link").live('click', function(e) {
-    // trs and tds are things that we want to link to the edit page
-    // if the click's target is a button for instance, we don't want to move the user.
-    if ($(e.target).is('tr') || $(e.target).is('td') || $(e.target).is('div.bar')) {
-      window.location.href = $(this).attr("data-link");
-    };
-  });
-
-  // On the list page, the checkbox in th table's header toggles all the checkboxes underneath it.
-  $("table.table input.checkbox.toggle").live('click', function() {
-    var checked_status = $(this).is(":checked");
-    $("td.action.select input.checkbox[name='bulk_ids[]']").each(function() {
-      $(this).attr('checked', checked_status);
-
-      if (checked_status) {
-        $(this).parent().addClass("checked");
-      } else {
-        $(this).parent().removeClass("checked");
-      }
-
-    });
-  });
-
 });
