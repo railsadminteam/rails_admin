650ee1770b0c1aae70d0a6bde1b7fa12e104de7f
Benoit Bénézech <benoit.benezech@gmail.com>
2011-08-05 18:34:40 +0200

    use image_path in CSS (sprocket)

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -46,26 +46,26 @@ module RailsAdmin
     def action_button link, text, icon=nil, options={}
       options.reverse_merge! :class => "button"
       link_to link, options do
-        image = image_tag "/assets/rails_admin/theme/activo/images/icons/#{icon}.png" if icon
+        image = image_tag(image_path("rails_admin/theme/activo/images/icons/#{icon}.png")) if icon
         [image, text].compact.join("\n").html_safe
       end.html_safe
     end
 
     # the icon shown beside every entry in the list view
     def action_icon link, icon, text
-      icon_path = "/assets/rails_admin/theme/activo/images/icons/24/%s.png"
+      icon_path = "rails_admin/theme/activo/images/icons/24/%s.png"
       icon_change = "this.src='#{icon_path}'"
       link_to link do
-        image_tag (icon_path % icon),
+        image_tag image_path(icon_path % icon),
           :alt => text, :title => text,
-          :onmouseout  => (icon_change % icon),
-          :onmouseover => (icon_change % "#{icon}-hover")
+          :onmouseout  => "this.src='#{image_path(icon_path % icon)}'",
+          :onmouseover => "this.src='#{image_path(icon_path % (icon.to_s + '-hover'))}'"
       end.html_safe
     end
 
     # Used for the icons in the admins very top right.
     def header_icon(image_name, title)
-      image_tag "/assets/rails_admin/theme/activo/images/session/#{image_name}.png", :alt => title, :title => title
+      image_tag image_path("rails_admin/theme/activo/images/session/#{image_name}.png"), :alt => title, :title => title
     end
 
     # Used for the history entries in the sidebar
