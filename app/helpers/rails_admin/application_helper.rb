0c6a19160c7a43c2688492591d8850165fa5d634
Benoit Bénézech <benoit.benezech@gmail.com>
2011-10-06 21:16:53 +0200

    Breadcrumb cleaning.

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -119,83 +119,57 @@ module RailsAdmin
     end
 
     # Creative whitespace:
-    ViewType   =          Struct.new(:parent,    :type,   :authorization, :path_method)
+    ViewType   =           Struct.new(:parent,    :type,   :authorization, :path_method)
     VIEW_TYPES = {
       :delete         => ViewType.new(:show,      :object, :delete),
-      :history        => ViewType.new(:show,      :object, nil,            :history_object),
+      :for_object     => ViewType.new(:show,      :object, nil,            :history_object),
       :edit           => ViewType.new(:show,      :object, :edit),
       :show           => ViewType.new(:index,     :object, nil),
       :export         => ViewType.new(:index,     :model,  :export),
       :bulk_delete    => ViewType.new(:index,     :model,  :delete),
       :new            => ViewType.new(:index,     :model,  :new),
-      :model_history  => ViewType.new(:index,     :model,  nil,            :history_model),
+      :for_model      => ViewType.new(:index,     :model,  nil,            :history_model),
       :index          => ViewType.new(:dashboard, :model,  :index),
-      :dashboard       => ViewType.new
+      :dashboard      => ViewType.new
     }
 
-    def breadcrumbs_for action, abstract_model, object
-      begin
-      view = case action
-        
-      when :for_object
-        :history
-      when :for_model
-        :model_history
-      else
-        action
-      end
-      
+    def breadcrumbs_for view, abstract_model, object
       return unless VIEW_TYPES[view]
-      
-      # create an array of all the names of the views we want breadcrumb links to
       views = []
-      #view = :model_history if view == :history && !@object
       parent = view
       begin
         views << parent
       end while parent = VIEW_TYPES[parent].parent
-
-      # get a breadcrumb for each view name
       breadcrumbs = views.reverse.map do |v|
         breadcrumb_for v, abstract_model, object, (v==view)
       end
-
       content_tag(:ul, :class => "breadcrumb") do
         breadcrumbs.join('<span class="divider">/</span>').html_safe
-      end
-      
-    rescue
-      raise [view.inspect, action.inspect, abstract_model.inspect, object.inspect, $!.to_s].join("\n\n\n") 
-    end
-      
+      end      
     end
 
     private
 
-      def breadcrumb_for view, abstract_model, object, active
-        
-        vt = VIEW_TYPES[view]
-        if authorized?(view, abstract_model, object)
-          css_classes = []
-          css_classes << "active" if active
-          config = RailsAdmin.config(abstract_model)
-          
-          content_tag(:li, :class => css_classes.join(' ')) do
-            path_method = vt.path_method || view
-            wording = case view
-              when :show
-                object.send(config.object_label_method)
-              when :index
-                config.label_plural
-              else
-                I18n.t("admin.breadcrumbs.#{view}").capitalize
-              end
-            link_to wording, self.send("#{path_method}_path")
+    def breadcrumb_for view, abstract_model, object, active
+      vt = VIEW_TYPES[view]
+      if authorized?(view, abstract_model, object)
+        css_classes = []
+        css_classes << "active" if active
+        config = RailsAdmin.config(abstract_model)
+        content_tag(:li, :class => css_classes.join(' ')) do
+          path_method = vt.path_method || view
+          wording = case view
+            when :show
+              object.send(config.object_label_method)
+            when :index
+              config.label_plural
+            else
+              I18n.t("admin.breadcrumbs.#{view}").capitalize
           end
-         end
+          link_to wording, self.send("#{path_method}_path")
+        end
       end
-
-
+    end
   end
 end
 
