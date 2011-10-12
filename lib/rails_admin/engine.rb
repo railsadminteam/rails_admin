20c41fbc1863878a7749ad56f20e11ccbf761e42
Benoit Bénézech <benoit.benezech@gmail.com>
2011-10-12 15:46:56 +0200

    added hook to compile rails_admin assets [ci skip]

--- lib/rails_admin/engine.rb
+++ lib/rails_admin/engine.rb
@@ -6,5 +6,8 @@ require 'rails_admin'
 module RailsAdmin
   class Engine < Rails::Engine
     isolate_namespace RailsAdmin
+    initializer "RailsAdmin precompile hook" do |app|
+      app.config.assets.precompile += ['rails_admin/rails_admin.js', 'rails_admin/rails_admin.css']
+    end
   end
 end
