87e5161abcd16c629a4283efed23502209c9da4a
Benoit Bénézech <benoit.benezech@gmail.com>
2011-11-03 14:38:14 +0100

    cleanup [ci skip]

--- spec/dummy_app/config/initializers/rails_admin.rb
+++ spec/dummy_app/config/initializers/rails_admin.rb
@@ -1,6 +1,3 @@
-#gem 'less-rails' # FIX ME. Remove from here and fix less-rails-bootstrap engine initializer
-#gem 'less-rails-bootstrap'
-
 RailsAdmin.config do |c|
   c.excluded_models << RelTest
   
