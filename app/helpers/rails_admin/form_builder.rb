6ffe0ecaa87dcdadb1d764319c50ee006b223d49
Benoit Bénézech <benoit.benezech@gmail.com>
2011-11-23 18:59:43 +0100

    milestone: JS fact. [ci skip]

--- app/helpers/rails_admin/form_builder.rb
+++ app/helpers/rails_admin/form_builder.rb
@@ -4,11 +4,17 @@ module RailsAdmin
   class FormBuilder < ::ActionView::Helpers::FormBuilder
     include ::NestedForm::BuilderMixin
     
-    def render action, model_config = @template.instance_variable_get(:@model_config), nested = false
-      model_config.send(action).with(:form => self, :object => @object, :view => @template).visible_groups.map do |fieldset|
+    def generate(options = {})
+      options.reverse_merge!({
+        :action => @template.controller.params[:action],
+        :model_config => @template.instance_variable_get(:@model_config),
+        :nested => false
+      })
+      
+      options[:model_config].send(options[:action]).with(:form => self, :object => @object, :view => @template).visible_groups.map do |fieldset|
         fieldset_for fieldset
       end.join.html_safe +
-      (nested ? '' : @template.render(:partial => 'submit_buttons'))
+      (options[:nested] ? '' : @template.render(:partial => 'submit_buttons'))
     end
 
     def fieldset_for fieldset
@@ -21,7 +27,7 @@ module RailsAdmin
     end
 
     def field_wrapper_for field
-      @template.content_tag(:div, :class => "clearfix field #{field.type_css_class} #{field.css_class} #{'error' if field.errors.present?}", :id => dom_id(field) + '_field') do
+      @template.content_tag(:div, :class => "clearfix field #{field.type_css_class} #{field.css_class} #{'error' if field.errors.present?}", :id => "#{dom_id(field)}_field") do
         label(field.method_name, field.label) +
         input_for(field)
       end
@@ -51,12 +57,31 @@ module RailsAdmin
       end
     end
     
+    def javascript_for(field, options = {}, &block)
+      %{
+        <script type="text/javascript">
+          jQuery(function($) {
+            if(!$("#{js_namespace(field)}").parents(".fields_blueprint").length) {
+              if(#{options[:modal] == false ? '!$("#modal").length' : 'true'}) {
+                #{@template.capture(&block)}
+              }
+            }
+          });
+        </script>
+      }.html_safe
+    end
+    
+    def js_namespace field
+      "#{(@template.controller.params[:modal] ? '#modal ' : '')}##{dom_id(field)}_field"
+    end
+    
     def dom_id field
-      (@dom_id ||= {})[field.name] ||= [
-        @object_name.sub('][', '_').sub(']', '').sub('[', '_'),
-        options[:index],
-        field.method_name
-      ].reject(&:blank?).join('_')
+      (@dom_id ||= {})[field.name] ||= 
+        [
+          @object_name.sub('][', '_').sub(']', '').sub('[', '_'),
+          options[:index],
+          field.method_name
+        ].reject(&:blank?).join('_')
     end
     
     def dom_name field
