96993f6ebfab8ab6e55b3813224c616c586b7a22
Erik Michaels-Ober <sferik@gmail.com>
2011-11-29 01:49:45 -0800

    Fix tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe RailsAdmin::ApplicationController do
   describe "#to_model_name" do
     it "handles classes nested in modules of the same name" do
-      controller.to_model_name("conversations~conversations").should eq("Conversations::Conversation")
+      controller.to_model_name("conversations~conversations").should eq("Conversations::Conversations")
     end
 
     it "should convert with singularity" do
