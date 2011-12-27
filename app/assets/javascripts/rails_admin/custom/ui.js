b9d6f7d600e7c20941eaa6064302b24eff8d4e40
Benoit Bénézech <benoit.benezech@gmail.com>
2011-12-27 16:31:37 +0100

    added wording for theming [ci skip]

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,1 +1,12 @@
-/* Meant to be overriden in user's app/assets/javascripts/rails_admin/custom directory. no content here */
+/* 
+  Meant to be overriden in user's app/assets/javascripts/rails_admin/custom directory. no content here 
+
+  Available resources:
+  
+  http://twitter.github.com/bootstrap/javascript.html
+  https://github.com/twitter/bootstrap/tree/master/js
+  http://jquery.com/
+  http://jqueryui.com/ (parts of it)
+  
+  rename to ui.js.coffee if needed.
+*/
