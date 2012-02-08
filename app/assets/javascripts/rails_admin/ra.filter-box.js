a2275ea0abf204ffff558ce9d2ef85ee1a28f8f9
Benoit Bénézech <benoit.benezech@gmail.com>
2012-02-08 18:56:13 +0100

    make filters look good & comfy

--- app/assets/javascripts/rails_admin/ra.filter-box.js
+++ app/assets/javascripts/rails_admin/ra.filter-box.js
@@ -8,7 +8,7 @@
       var operator_name = 'f[' +  field_name + '][' + index + '][o]';
       switch(field_type) {
         case 'boolean':
-          var control = '<select name="' + value_name + '">' +
+          var control = '<select class="input-small" name="' + value_name + '">' +
             '<option value="_discard">...</option>' +
             '<option value="true"' + (field_value == "true" ? 'selected="selected"' : '') + '>True</option>' +
             '<option value="false"' + (field_value == "false" ? 'selected="selected"' : '') + '>False</option>' +
@@ -20,7 +20,7 @@
         case 'date':
         case 'datetime':
         case 'timestamp':
-          var control = '<select class="switch-additionnal-fieldsets" name="' + operator_name + '">' +
+          var control = '<select class="switch-additionnal-fieldsets input-small" name="' + operator_name + '">' +
             '<option data-additional-fieldset="false" value="_discard">...</option>' +
             '<option data-additional-fieldset="false"' + (field_operator == "today"     ? 'selected="selected"' : '') + ' value="today">Today</option>' +
             '<option data-additional-fieldset="false"' + (field_operator == "yesterday" ? 'selected="selected"' : '') + ' value="yesterday">Yesterday</option>' +
@@ -33,27 +33,26 @@
             '<option data-additional-fieldset="false"' + (field_operator == "_not_null"  ? 'selected="selected"' : '') + ' value="_not_null">Is present</option>' +
             '<option data-additional-fieldset="false"' + (field_operator == "_null"    ? 'selected="selected"' : '') + ' value="_null" >Is blank</option>' +
           '</select>'
-          var additional_control = '<input class="additional-fieldset " style="display:' + (field_operator == "less_than" || field_operator == "more_than" || field_operator == "mmddyyyy" ? 'block' : 'none') + ';" type="text" name="' + value_name + '" value="' + field_value + '" /> ';
+          var additional_control = '<input class="additional-fieldset input-small" style="display:' + (field_operator == "less_than" || field_operator == "more_than" || field_operator == "mmddyyyy" ? 'inline-block' : 'none') + ';" type="text" name="' + value_name + '" value="' + field_value + '" /> ';
           break;
         case 'enum':
           var field_options = $('<div/>').html(field_options).text(); // entities decode
-          var control = '<span class="switch-select">' + 
-            '<select style="display:' + (multiple_values ? 'none' : 'block') + '" ' + (multiple_values ? '' : 'name="' + value_name + '"') + ' data-name="' + value_name + '" class="select-single">' +
+          var control = '<select style="display:' + (multiple_values ? 'none' : 'inline-block') + '" ' + (multiple_values ? '' : 'name="' + value_name + '"') + ' data-name="' + value_name + '" class="select-single input-small">' +
               '<option value="_discard">...</option>' +
               '<option ' + (field_value == "_present" ? 'selected="selected"' : '') + ' value="_present">Is present</option>' +
               '<option ' + (field_value == "_blank"   ? 'selected="selected"' : '') + ' value="_blank">Is blank</option>' +
               '<option disabled="disabled">---------</option>' +
               field_options +
             '</select>' + 
-            '<select multiple="multiple" style="display:' + (multiple_values ? 'block' : 'none') + '" ' + (multiple_values ? 'name="' + value_name + '[]"' : '') + ' data-name="' + value_name + '[]" class="select-multiple">' +
+            '<select multiple="multiple" style="display:' + (multiple_values ? 'inline-block' : 'none') + '" ' + (multiple_values ? 'name="' + value_name + '[]"' : '') + ' data-name="' + value_name + '[]" class="select-multiple input-small">' +
               field_options +
-            '</select>' +
-          '</span>';
+            '</select> ' +
+            '<a href="#" class="switch-select"><i class="icon-' + (multiple_values ? 'minus' : 'plus') + '"></i></a>';
         break;
         case 'string':
         case 'text':
         case 'belongs_to_association':
-          var control = '<select class="switch-additionnal-fieldsets" value="' + field_operator + '" name="' + operator_name + '">' +
+          var control = '<select class="switch-additionnal-fieldsets input-small" value="' + field_operator + '" name="' + operator_name + '">' +
             '<option data-additional-fieldset="true"'  + (field_operator == "like"        ? 'selected="selected"' : '') + ' value="like">Contains</option>' +
             '<option data-additional-fieldset="true"'  + (field_operator == "is"          ? 'selected="selected"' : '') + ' value="is">Is exactly</option>' +
             '<option data-additional-fieldset="true"'  + (field_operator == "starts_with" ? 'selected="selected"' : '') + ' value="starts_with">Starts with</option>' +
@@ -62,28 +61,25 @@
             '<option data-additional-fieldset="false"' + (field_operator == "_present"    ? 'selected="selected"' : '') + ' value="_present">Is present</option>' +
             '<option data-additional-fieldset="false"' + (field_operator == "_blank"      ? 'selected="selected"' : '') + ' value="_blank">Is blank</option>' +
           '</select>'
-          var additional_control = '<input class="additional-fieldset" style="display:' + (field_operator == "_blank" || field_operator == "_present" ? 'none' : 'block') + ';" type="text" name="' + value_name + '" value="' + field_value + '" /> ';
+          var additional_control = '<input class="additional-fieldset input-small" style="display:' + (field_operator == "_blank" || field_operator == "_present" ? 'none' : 'inline-block') + ';" type="text" name="' + value_name + '" value="' + field_value + '" /> ';
           break;
         default:
-          var control = '<input type="text" name="' + value_name + '" value="' + field_value + '"/> ';
+          var control = '<input type="text" class="input-small" name="' + value_name + '" value="' + field_value + '"/> ';
           break;
       }
 
-      
-      var content = '<div class="row filter clearfix">' +
-          '<span class="span3">' +
-            '<span class="btn info delete">' + field_label + '</span>' +
-          '</span>' +
-          '<span class="span3">'+
-            control +
-          '</span>'+ 
-          (additional_control ? '<span class="span2">' + additional_control + '</span>' : '') +
-        '</div>'
+      var content = '<p class="filter form-search">' +
+        '<span class="label label-info" style="padding:7px"><a href="#" class="delete"><i class="icon-trash icon-white"></i></a> ' + field_label + '</span> ' +
+        control + " " +
+        (additional_control || '') +
+      '</div> ';
       $('#filters_box').append(content);
+      $("hr.filters_box:hidden").show('slow');
     }
   }
 
-  $("#filters a").live('click', function() {
+  $("#filters a").live('click', function(e) {
+    e.preventDefault();
     $.filters.append(
       $(this).data('field-label'),
       $(this).data('field-name'),
@@ -96,25 +92,29 @@
     );
   });
 
-  $('#filters_box .delete').live('click', function() {
+  $('#filters_box .delete').live('click', function(e) {
+    e.preventDefault();
     form = $(this).parents('form');
     $(this).parents('.filter').remove();
+    !$("#filters_box").children().length && $("hr.filters_box:visible").hide('slow');
     form.submit();
   });
 
-  $('#filters_box .switch-select').live('dblclick', function() {
-    var selected_select = $(this).children('select:visible');
-    var not_selected_select = $(this).children('select:hidden');
+  $('#filters_box .switch-select').live('click', function(e) {
+    e.preventDefault();
+    var selected_select = $(this).siblings('select:visible');
+    var not_selected_select = $(this).siblings('select:hidden');
     not_selected_select.attr('name', not_selected_select.data('name')).show('slow');
     selected_select.attr('name', null).hide('slow');
+    $(this).find('i').toggleClass("icon-plus icon-minus")
   });
 
   $('#filters_box .switch-additionnal-fieldsets').live('change', function() {
     var selected_option = $(this).find('option:selected');
     if($(selected_option).data('additional-fieldset')) {
-      $(this).parent().siblings().children('.additional-fieldset').show('slow');
+      $(this).siblings('.additional-fieldset').show('slow');
     } else {
-      $(this).parent().siblings().children('.additional-fieldset').hide('slow');
+      $(this).siblings('.additional-fieldset').hide('slow');
     }
   });
 })( jQuery );
