b3856808d7173d76c248fa1a69b03cdff677645d
Benoit Bénézech <benoit.benezech@gmail.com>
2012-02-22 15:31:28 +0100

    update pjax, update form.pjax, th.pjax and a.pjax support for non-pjax browsers [ci skip]

--- app/assets/javascripts/rails_admin/jquery.pjax.js
+++ app/assets/javascripts/rails_admin/jquery.pjax.js
@@ -24,35 +24,61 @@
 //
 // Returns the jQuery object
 $.fn.pjax = function( container, options ) {
-  if ( options )
-    options.container = container
-  else
-    options = $.isPlainObject(container) ? container : {container:container}
+  options = optionsFor(container, options)
+  return this.live('click', function(event){
+    return handleClick(event, options)
+  })
+}
 
-  // We can't persist $objects using the history API so we must use
-  // a String selector. Bail if we got anything else.
-  if ( options.container && typeof options.container !== 'string' ) {
-    throw "pjax container must be a string selector!"
-    return false
+// Public: pjax on click handler
+//
+// Exported as $.pjax.click.
+//
+// event   - "click" jQuery.Event
+// options - pjax options
+//
+// Examples
+//
+//   $('a').live('click', $.pjax.click)
+//   // is the same as
+//   $('a').pjax()
+//
+//  $(document).on('click', 'a', function(event) {
+//    var container = $(this).closest('[data-pjax-container]')
+//    return $.pjax.click(event, container)
+//  })
+//
+// Returns false if pjax runs, otherwise nothing.
+function handleClick(event, container, options) {
+  options = optionsFor(container, options)
+
+  var link = event.currentTarget
+
+  // Middle click, cmd click, and ctrl click should open
+  // links in a new tab as normal.
+  if ( event.which > 1 || event.metaKey )
+    return
+
+  // Ignore cross origin links
+  if ( location.protocol !== link.protocol || location.host !== link.host )
+    return
+
+  // Ignore anchors on the same page
+  if ( link.hash && link.href.replace(link.hash, '') ===
+       location.href.replace(location.hash, '') )
+    return
+
+  var defaults = {
+    url: link.href,
+    container: $(link).attr('data-pjax'),
+    clickedElement: $(link),
+    fragment: null
   }
 
-  return this.live('click', function(event){
-    // Middle click, cmd click, and ctrl click should open
-    // links in a new tab as normal.
-    if ( event.which > 1 || event.metaKey )
-      return true
+  $.pjax($.extend({}, defaults, options))
 
-    var defaults = {
-      url: (this.href || $(this).attr('data-href')),
-      container: $(this).attr('data-pjax'),
-      clickedElement: $(this),
-      fragment: null
-    }
-
-    $.pjax($.extend({}, defaults, options))
-
-    event.preventDefault()
-  })
+  event.preventDefault()
+  return false
 }
 
 
@@ -64,7 +90,7 @@ $.fn.pjax = function( container, options ) {
 //
 // Accepts these extra keys:
 //
-// container - Where to stick the response body. Must be a String.
+// container - Where to stick the response body.
 //             $(container).html(xhr.responseBody)
 //      push - Whether to pushState the URL. Defaults to true (of course).
 //   replace - Want to use replaceState instead? That's cool.
@@ -76,56 +102,116 @@ $.fn.pjax = function( container, options ) {
 //
 // Returns whatever $.ajax returns.
 var pjax = $.pjax = function( options ) {
-  var $container = $(options.container),
-      success = options.success || $.noop
-
-  // We don't want to let anyone override our success handler.
-  delete options.success
-
-  // We can't persist $objects using the history API so we must use
-  // a String selector. Bail if we got anything else.
-  if ( typeof options.container !== 'string' )
-    throw "pjax container must be a string selector!"
-
-  options = $.extend(true, {}, pjax.defaults, options)
+  options = $.extend(true, {}, $.ajaxSettings, pjax.defaults, options)
 
   if ( $.isFunction(options.url) ) {
     options.url = options.url()
   }
 
-  options.context = $container
-  options.success = function(data){
+  // DEPRECATED: Save references to original event callbacks. However,
+  // listening for custom pjax:* events is prefered.
+  var oldBeforeSend = options.beforeSend,
+      oldComplete   = options.complete,
+      oldSuccess    = options.success,
+      oldError      = options.error
+
+  options.context = findContainerFor(options.container)
+
+  var timeoutTimer
+
+  options.beforeSend = function(xhr, settings) {
+    var context = this
+
+    if (settings.timeout > 0) {
+      timeoutTimer = setTimeout(function() {
+        var event = $.Event('pjax:timeout')
+        context.trigger(event, [xhr, options])
+        if (event.result !== false)
+          xhr.abort('timeout')
+      }, settings.timeout)
+
+      // Clear timeout setting so jquerys internal timeout isn't invoked
+      settings.timeout = 0
+    }
+
+    xhr.setRequestHeader('X-PJAX', 'true')
+
+    var result
+
+    // DEPRECATED: Invoke original `beforeSend` handler
+    if (oldBeforeSend) {
+      result = oldBeforeSend.apply(this, arguments)
+      if (result === false) return false
+    }
+
+    var event = $.Event('pjax:beforeSend')
+    this.trigger(event, [xhr, settings])
+    result = event.result
+    if (result === false) return false
+
+    this.trigger('pjax:start', [xhr, options])
+    // start.pjax is deprecated
+    this.trigger('start.pjax', [xhr, options])
+  }
+
+  options.complete = function(xhr, textStatus) {
+    if (timeoutTimer)
+      clearTimeout(timeoutTimer)
+
+    // DEPRECATED: Invoke original `complete` handler
+    if (oldComplete) oldComplete.apply(this, arguments)
+
+    this.trigger('pjax:complete', [xhr, textStatus, options])
+
+    this.trigger('pjax:end', [xhr, options])
+    // end.pjax is deprecated
+    this.trigger('end.pjax', [xhr, options])
+  }
+
+  options.error = function(xhr, textStatus, errorThrown) {
+    // DEPRECATED: Invoke original `error` handler
+    if (oldError) oldError.apply(this, arguments)
+
+    var event = $.Event('pjax:error')
+    this.trigger(event, [xhr, textStatus, errorThrown, options])
+    if (textStatus !== 'abort' && event.result !== false)
+      window.location = options.url
+  }
+
+  options.success = function(data, status, xhr) {
+    var title, oldTitle = document.title
+
     if ( options.fragment ) {
       // If they specified a fragment, look for it in the response
       // and pull it out.
-      var $fragment = $(data).find(options.fragment)
-      if ( $fragment.length )
-        data = $fragment.children()
-      else
+      var html = $('<html>').html(data)
+      var $fragment = html.find(options.fragment)
+      if ( $fragment.length ) {
+        this.html($fragment.contents())
+
+        // If there's a <title> tag in the response, use it as
+        // the page's title. Otherwise, look for data-title and title attributes.
+        title = html.find('title').text() || $fragment.attr('title') || $fragment.data('title')
+      } else {
         return window.location = options.url
+      }
     } else {
-        // If we got no data or an entire web page, go directly
-        // to the page and let normal error handling happen.
-        if ( !$.trim(data) || /<html/i.test(data) )
-          return window.location = options.url
+      // If we got no data or an entire web page, go directly
+      // to the page and let normal error handling happen.
+      if ( !$.trim(data) || /<html/i.test(data) )
+        return window.location = options.url
+
+      this.html(data)
+
+      // If there's a <title> tag in the response, use it as
+      // the page's title.
+      title = this.find('title').remove().text()
     }
 
-    // Make it happen.
-    this.html(data)
-
-    // If there's a <title> tag in the response, use it as
-    // the page's title.
-    var oldTitle = document.title,
-        title = $.trim( this.find('title').remove().text() )
-    if ( title ) document.title = title
-
-    // No <title>? Fragment? Look for data-title and title attributes.
-    if ( !title && options.fragment ) {
-      title = $fragment.attr('title') || $fragment.data('title')
-    }
+    if ( title ) document.title = $.trim(title)
 
     var state = {
-      pjax: options.container,
+      pjax: this.selector,
       fragment: options.fragment,
       timeout: options.timeout
     }
@@ -136,6 +222,7 @@ var pjax = $.pjax = function( options ) {
       state.url = options.url + (/\?/.test(options.url) ? "&" : "?") + query
 
     if ( options.replace ) {
+      pjax.active = true
       window.history.replaceState(state, document.title, options.url)
     } else if ( options.push ) {
       // this extra replaceState before first push ensures good back
@@ -159,9 +246,12 @@ var pjax = $.pjax = function( options ) {
       window.location.href = hash
     }
 
-    // Invoke their success handler if they gave us one.
-    success.apply(this, arguments)
+    // DEPRECATED: Invoke original `success` handler
+    if (oldSuccess) oldSuccess.apply(this, arguments)
+
+    this.trigger('pjax:success', [data, status, xhr, options])
   }
+
 
   // Cancel the current request if we're already pjaxing
   var xhr = pjax.xhr
@@ -178,8 +268,68 @@ var pjax = $.pjax = function( options ) {
 }
 
 
+// Internal: Build options Object for arguments.
+//
+// For convenience the first parameter can be either the container or
+// the options object.
+//
+// Examples
+//
+//   optionsFor('#container')
+//   // => {container: '#container'}
+//
+//   optionsFor('#container', {push: true})
+//   // => {container: '#container', push: true}
+//
+//   optionsFor({container: '#container', push: true})
+//   // => {container: '#container', push: true}
+//
+// Returns options Object.
+function optionsFor(container, options) {
+  // Both container and options
+  if ( container && options )
+    options.container = container
+
+  // First argument is options Object
+  else if ( $.isPlainObject(container) )
+    options = container
+
+  // Only container
+  else
+    options = {container: container}
+
+  // Find and validate container
+  if (options.container)
+    options.container = findContainerFor(options.container)
+
+  return options
+}
+
+// Internal: Find container element for a variety of inputs.
+//
+// Because we can't persist elements using the history API, we must be
+// able to find a String selector that will consistently find the Element.
+//
+// container - A selector String, jQuery object, or DOM Element.
+//
+// Returns a jQuery object whose context is `document` and has a selector.
+function findContainerFor(container) {
+  container = $(container)
+
+  if ( !container.length ) {
+    throw "no pjax container for " + container.selector
+  } else if ( container.selector !== '' && container.context === document ) {
+    return container
+  } else if ( container.attr('id') ) {
+    return $('#' + container.attr('id'))
+  } else {
+    throw "cant get selector for pjax container!"
+  }
+}
+
+
 pjax.defaults = {
-  timeout: 4000,
+  timeout: 650,
   push: true,
   replace: false,
   // We want the browser to maintain two separate internal caches: one for
@@ -187,26 +337,11 @@ pjax.defaults = {
   // adding this secret parameter, some browsers will often confuse the two.
   data: { _pjax: true },
   type: 'GET',
-  dataType: 'html',
-  beforeSend: function(xhr){
-    this.trigger('pjax:start', [xhr, pjax.options])
-    // start.pjax is deprecated
-    this.trigger('start.pjax', [xhr, pjax.options])
-    xhr.setRequestHeader('X-PJAX', 'true')
-  },
-  error: function(xhr, textStatus, errorThrown){
-    if ( textStatus !== 'abort' ) {
-      // console.log(xhr, textStatus, errorThrown)
-      window.location = pjax.options.url
-    }
-     
-  },
-  complete: function(xhr){
-    this.trigger('pjax:end', [xhr, pjax.options])
-    // end.pjax is deprecated
-    this.trigger('end.pjax', [xhr, pjax.options])
-  }
+  dataType: 'html'
 }
+
+// Export $.pjax.click
+pjax.click = handleClick
 
 
 // Used to detect initial (useless) popstate.
@@ -251,15 +386,45 @@ if ( $.inArray('state', $.event.props) < 0 )
 // Is pjax supported by this browser?
 $.support.pjax =
   window.history && window.history.pushState && window.history.replaceState
-  // pushState isn't reliable on iOS yet.
-  && !navigator.userAgent.match(/(iPod|iPhone|iPad|WebApps\/.+CFNetwork)/)
+  // pushState isn't reliable on iOS until 5.
+  && !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]|WebApps\/.+CFNetwork)/)
 
 
 // Fall back to normalcy for older browsers.
 if ( !$.support.pjax ) {
   $.pjax = function( options ) {
-    window.location = $.isFunction(options.url) ? options.url() : options.url
+    var url = $.isFunction(options.url) ? options.url() : options.url,
+        method = options.type ? options.type.toUpperCase() : 'GET'
+
+    var form = $('<form>', {
+      method: method === 'GET' ? 'GET' : 'POST',
+      action: url,
+      style: 'display:none'
+    })
+
+    if (method !== 'GET' && method !== 'POST') {
+      form.append($('<input>', {
+        type: 'hidden',
+        name: '_method',
+        value: method.toLowerCase()
+      }))
+    }
+
+    var data = options.data
+    if (typeof data === 'string') {
+      $.each(data.split('&'), function(index, value) {
+        var pair = value.split('=')
+        form.append($('<input>', {type: 'hidden', name: pair[0], value: pair[1]}))
+      })
+    } else if (typeof data === 'object') {
+      for (key in data)
+        form.append($('<input>', {type: 'hidden', name: key, value: data[key]}))
+    }
+
+    $(document.body).append(form)
+    form.submit()
   }
+  $.pjax.click = $.noop
   $.fn.pjax = function() { return this }
 }
 
