bf753736f51a1c1d2d8d955c3c89ad922aaeb536
Erik Michaels-Ober <sferik@gmail.com>
2012-02-23 10:48:20 -0800

    Don't raise exceptions on mass assignment protection for Active Record models

--- spec/dummy_app/config/environments/test.rb
+++ spec/dummy_app/config/environments/test.rb
@@ -30,7 +30,7 @@ DummyApp::Application.configure do
   config.action_mailer.delivery_method = :test
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
