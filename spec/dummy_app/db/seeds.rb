1117158f9699942a5dda647dce7d4127527ec2da
Tim Kurvers <tim@moonsphere.net>
2012-04-07 23:01:31 +0200

    Ensure seeds are applied in an ORM-agnostic manner

--- spec/dummy_app/db/seeds.rb
+++ spec/dummy_app/db/seeds.rb
@@ -1,18 +1,29 @@
 require 'mlb'
 
-User.create(:email => 'username@example.com', :password => 'password', :password_confirmation => 'password')
+AgnosticUser     = RailsAdmin::AbstractModel.new(User)
+AgnosticLeague   = RailsAdmin::AbstractModel.new(League)
+AgnosticDivision = RailsAdmin::AbstractModel.new(Division)
+AgnosticTeam     = RailsAdmin::AbstractModel.new(Team)
+AgnosticPlayer   = RailsAdmin::AbstractModel.new(Player)
+
+AgnosticUser.new(:email => 'username@example.com', :password => 'password', :password_confirmation => 'password').save
 
 MLB::Team.all.each do |mlb_team|
-  unless league = League.where(:name => mlb_team.league).first
-    league = League.create(:name => mlb_team.league)
+  unless league = AgnosticLeague.where(:name => mlb_team.league).first
+    league = AgnosticLeague.new(:name => mlb_team.league)
+    league.save
   end
-  unless division = Division.where(:name => mlb_team.division).first
-    division = Division.create(:name => mlb_team.division, :league => league)
+  unless division = AgnosticDivision.where(:name => mlb_team.division).first
+    division = AgnosticDivision.new(:name => mlb_team.division, :league => league)
+    division.save
   end
-  unless team = Team.where(:name => mlb_team.name).first
-    team = Team.create(:name => mlb_team.name, :logo_url => mlb_team.logo_url, :manager => mlb_team.manager, :ballpark => mlb_team.ballpark, :mascot => mlb_team.mascot, :founded => mlb_team.founded, :wins => mlb_team.wins, :losses => mlb_team.losses, :win_percentage => ("%.3f" % (mlb_team.wins.to_f / (mlb_team.wins + mlb_team.losses))).to_f, :division => division)
+  unless team = AgnosticTeam.where(:name => mlb_team.name).first
+    team = AgnosticTeam.new(:name => mlb_team.name, :logo_url => mlb_team.logo_url, :manager => mlb_team.manager, :ballpark => mlb_team.ballpark, :mascot => mlb_team.mascot, :founded => mlb_team.founded, :wins => mlb_team.wins, :losses => mlb_team.losses, :win_percentage => ("%.3f" % (mlb_team.wins.to_f / (mlb_team.wins + mlb_team.losses))).to_f, :division => division)
+    team.save
   end
   mlb_team.players.reject{|player| player.number.nil?}.each do |player|
-    Player.create(:name => player.name, :number => player.number, :position => player.position, :team => team)
+    AgnosticPlayer.new(:name => player.name, :number => player.number, :position => player.position, :team => team).save
   end
 end
+
+puts "Seeded #{AgnosticLeague.count} leagues, #{AgnosticDivision.count} divisions, #{AgnosticTeam.count} teams and #{AgnosticPlayer.count} players"
