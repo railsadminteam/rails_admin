08b7af334df4e95a876c917c336079113772b2af
Eric Hochberger <eric@mediavine.com>
2012-04-09 09:50:17 -0400

    Add Modal Config Section

---
+++ lib/rails_admin/config/sections/modal.rb
@@ -0,0 +1,10 @@
+require 'rails_admin/config/sections/edit'
+
+module RailsAdmin
+  module Config
+    module Sections
+      class Modal < RailsAdmin::Config::Sections::Edit
+      end
+    end
+  end
+end
