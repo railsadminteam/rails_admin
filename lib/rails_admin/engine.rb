152dc34b6cd73a54cc739a3074a006ffed918dc4
Adrian Macneil <adrian@crescendo.net.nz>
2012-05-16 11:59:23 +1000

    Trigger asset precompile hook even if initialize_on_precompile is set to false

--- lib/rails_admin/engine.rb
+++ lib/rails_admin/engine.rb
@@ -11,7 +11,7 @@ require 'rails_admin'
 module RailsAdmin
   class Engine < Rails::Engine
     isolate_namespace RailsAdmin
-    initializer "RailsAdmin precompile hook" do |app|
+    initializer "RailsAdmin precompile hook", :group => :assets do |app|
       app.config.assets.precompile += ['rails_admin/rails_admin.js', 'rails_admin/rails_admin.css', 'rails_admin/jquery.colorpicker.js', 'rails_admin/jquery.colorpicker.css']
     end
 
