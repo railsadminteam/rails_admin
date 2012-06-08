18dfcc436b6772dfa8339dda52f800a321ba0f16
M.Shibuya <mit.shibuya@gmail.com>
2012-06-08 11:47:57 +0900

    Check availability of show action on delete. Fixes #1156

--- lib/rails_admin/abstract_model.rb
+++ lib/rails_admin/abstract_model.rb
@@ -80,6 +80,21 @@ module RailsAdmin
       model.where(conditions)
     end
 
+    def each_associated_children(object)
+      associations.each do |association|
+        case association[:type]
+        when :has_one
+          if child = object.send(association[:name])
+            yield(association, child)
+          end
+        when :has_many
+          object.send(association[:name]).each do |child|
+            yield(association, child)
+          end
+        end
+      end
+    end
+
     private
 
     def get_filtering_duration(operator, value)
--- spec/integration/basic/delete/rails_admin_basic_delete_spec.rb
+++ spec/integration/basic/delete/rails_admin_basic_delete_spec.rb
@@ -6,12 +6,17 @@ describe "RailsAdmin Basic Delete" do
 
   describe "delete" do
     before(:each) do
-      @player = FactoryGirl.create :player
+      @draft = FactoryGirl.create :draft
+      @player = @draft.player
+      @comment = @player.comments.create
       visit delete_path(:model_name => "player", :id => @player.id)
     end
 
     it "should show \"Delete model\"" do
       should have_content("delete this player")
+      should have_link(@player.name, :href => "/admin/player/#{@player.id}")
+      should have_link("Draft ##{@draft.id}", :href => "/admin/draft/#{@draft.id}")
+      should have_link("Comment ##{@comment.id}", :href => "/admin/comment/#{@comment.id}")
     end
   end
 
@@ -22,6 +27,27 @@ describe "RailsAdmin Basic Delete" do
 
     it "should raise NotFound" do
       page.driver.status_code.should eql(404)
+    end
+  end
+
+  describe "with show action disabled" do
+    before(:each) do
+      RailsAdmin.config.actions do
+        dashboard
+        index
+        delete
+      end
+      @draft = FactoryGirl.create :draft
+      @player = @draft.player
+      @comment = @player.comments.create
+      visit delete_path(:model_name => "player", :id => @player.id)
+    end
+
+    it "should show \"Delete model\"" do
+      should have_content("delete this player")
+      should_not have_selector("a[href=\"/admin/player/#{@player.id}\"]")
+      should_not have_selector("a[href=\"/admin/draft/#{@draft.id}\"]")
+      should_not have_selector("a[href=\"/admin/comment/#{@comment.id}\"]")
     end
   end
 end
