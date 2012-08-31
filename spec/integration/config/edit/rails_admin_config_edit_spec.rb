936337d8b574a339ffb4d79988c7df6d2b45e681
M.Shibuya <mit.shibuya@gmail.com>
2012-08-31 21:00:18 +0900

    Prevent unintentional change of datetime fields in non-UTC timezones. Fixes #1301

--- spec/integration/config/edit/rails_admin_config_edit_spec.rb
+++ spec/integration/config/edit/rails_admin_config_edit_spec.rb
@@ -624,6 +624,16 @@ describe "RailsAdmin Config DSL Edit Section" do
         @record = RailsAdmin::AbstractModel.new("FieldTest").first
         @record.datetime_field.to_s(:rfc822).should eql(@time.to_s(:rfc822))
       end
+
+      it "should do round-trip saving properly with non-UTC timezones" do
+        Time.zone = 'Vienna'
+        time = Time.zone.parse('2012-09-01 12:00:00 +02:00')
+        @record = FieldTest.create!(:datetime_field => time)
+        visit edit_path(:model_name => "field_test", :id => @record.id)
+        click_button "Save"
+        @record = RailsAdmin::AbstractModel.new("FieldTest").first
+        @record.datetime_field.should == time
+      end
     end
 
     describe "a timestamp field", :active_record => true do
