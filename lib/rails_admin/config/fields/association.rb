8cd4d3a340158e0aa5f37594b7546a62f6941eee
Benoit Bénézech <benoit.benezech@gmail.com>
2012-09-13 17:14:27 +0200

    refactoring around show action linking

--- lib/rails_admin/config/fields/association.rb
+++ lib/rails_admin/config/fields/association.rb
@@ -21,8 +21,7 @@ module RailsAdmin
             amc = polymorphic? ? RailsAdmin.config(associated) : associated_model_config # perf optimization for non-polymorphic associations
             am = amc.abstract_model
             wording = associated.send(amc.object_label_method)
-            can_see = v.authorized?(:show, am, associated) && !am.embedded?
-            can_see = can_see && (show_action = RailsAdmin::Config::Actions.find(:show, { :controller => v.controller, :abstract_model => am, :object => associated }))
+            can_see = !am.embedded? && (show_action = v.action(:show, am, associated))
             can_see ? v.link_to(wording, v.url_for(:action => show_action.action_name, :model_name => am.to_param, :id => associated.id), :class => 'pjax') : wording
           end.to_sentence.html_safe
         end
