6c8e7ce36c554684f17bb0ad61437dc0fe760a8d
Benoit Bénézech <benoit.benezech@gmail.com>
2012-10-20 15:10:22 +0200

    fix for modals save and Mongoid (https://groups.google.com/forum/#!msg/mongoid/MaXFVw7D_4s/T3sl6Flg428J)

--- lib/rails_admin/config/actions/edit.rb
+++ lib/rails_admin/config/actions/edit.rb
@@ -41,7 +41,7 @@ module RailsAdmin
                 @auditing_adapter && @auditing_adapter.update_object(@abstract_model, @object, @cached_assocations_hash, associations_hash, @modified_assoc, @old_object, _current_user)
                 respond_to do |format|
                   format.html { redirect_to_on_success }
-                  format.js { render :json => { :id => @object.id, :label => @model_config.with(:object => @object).object_label } }
+                  format.js { render :json => { :id => @object.id.to_s, :label => @model_config.with(:object => @object).object_label } }
                 end
               else
                 handle_save_error :edit
