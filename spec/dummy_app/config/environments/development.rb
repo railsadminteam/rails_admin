02ae51456cf937e5c516a772a1c076b469450f5d
M.Shibuya <mit.shibuya@gmail.com>
2013-04-21 18:56:21 +0900

    Fix deprecations in config

--- spec/dummy_app/config/environments/development.rb
+++ spec/dummy_app/config/environments/development.rb
@@ -5,9 +5,7 @@ DummyApp::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  config.eager_load = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -24,10 +22,6 @@ DummyApp::Application.configure do
 
   # Raise exception on mass assignment protection for Active Record models
   # config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5 if CI_ORM == :active_record
 
   # Do not compress assets
   config.assets.compress = false
