e7269728f1a82e47beef4471616be5bfb01ae7d6
Michael Nikitochkin <miry.sof@gmail.com>
2013-04-26 22:15:43 +0300

    Fixed typo

--- lib/rails_admin/config/fields/factories/enum.rb
+++ lib/rails_admin/config/fields/factories/enum.rb
@@ -7,7 +7,7 @@ RailsAdmin::Config::Fields.register_factory do |parent, properties, fields|
   _method_name = "#{properties[:name]}_enum"
 
   #NOTICE: _method_name could be `to_enum` and this method defined in Object.
-  if  !Object.respond_to? _method_name && \
+  if !Object.respond_to?(_method_name) && \
       (_model.respond_to?(_method_name) || \
           _model.method_defined?(_method_name))
     fields << RailsAdmin::Config::Fields::Types::Enum.new(parent, properties[:name], properties)
