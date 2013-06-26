967efc6f875c5f2075c697e22e894cad2fbc99b1
M.Shibuya <mit.shibuya@gmail.com>
2013-06-26 14:32:23 +0900

    Create separate field types for richtext editors

--- lib/rails_admin/config/fields/types/text.rb
+++ lib/rails_admin/config/fields/types/text.rb
@@ -8,76 +8,22 @@ module RailsAdmin
           # Register field type for the type loader
           RailsAdmin::Config::Fields::Types::register(self)
 
-          # CKEditor is disabled by default
-          register_instance_option :ckeditor do
-            false
+          [:ckeditor, :ckeditor_base_location, :ckeditor_config_js, :ckeditor_location].each do |key|
+            register_deprecated_instance_option key do
+              raise "The 'field(:foo){ ckeditor true }' style DSL is deprecated. Please use 'field :foo, :ck_editor' instead."
+            end
           end
 
-          # If you want to have a different toolbar configuration for CKEditor
-          # create your own custom config.js and override this configuration
-          register_instance_option :ckeditor_config_js do
-            nil
+          [:codemirror, :codemirror_assets, :codemirror_config, :codemirror_css_location, :codemirror_js_location].each do |key|
+            register_deprecated_instance_option key do
+              raise "The 'field(:foo){ codemirror true }' style DSL is deprecated. Please use 'field :foo, :code_mirror' instead."
+            end
           end
 
-          #Use this if you want to point to a cloud instances of CKeditor
-          register_instance_option :ckeditor_location do
-            nil
-          end
-
-          #Use this if you want to point to a cloud instances of the base CKeditor
-          register_instance_option :ckeditor_base_location do
-            "#{Rails.application.config.assets.prefix}/ckeditor/"
-          end
-
-          # Codemirror is disabled by default and CKEditor takes precedence
-          register_instance_option :codemirror do
-            false
-          end
-
-          #Pass the theme and mode for Codemirror
-          register_instance_option :codemirror_config do
-            {
-              :mode => 'css',
-              :theme => 'night'
-            }
-          end
-
-          #Pass the location of the theme and mode for Codemirror
-          register_instance_option :codemirror_assets do
-            {
-              :mode => '/assets/codemirror/modes/css.js',
-              :theme => '/assets/codemirror/themes/night.css'
-            }
-          end
-
-          #Use this if you want to point to a cloud instances of CodeMirror
-          register_instance_option :codemirror_js_location do
-            '/assets/codemirror.js'
-          end
-
-          #Use this if you want to point to a cloud instances of CodeMirror
-          register_instance_option :codemirror_css_location do
-            '/assets/codemirror.css'
-          end
-
-          # bootstrap_wysihtml5
-          register_instance_option :bootstrap_wysihtml5 do
-            false
-          end
-
-          # If you want to have a different toolbar configuration for wysihtml5
-          # you can use a Ruby hash to configure these options:
-          # https://github.com/jhollingworth/bootstrap-wysihtml5/#advanced
-          register_instance_option :bootstrap_wysihtml5_config_options do
-            nil
-          end
-
-          register_instance_option :bootstrap_wysihtml5_css_location do
-            '/assets/bootstrap-wysihtml5.css'
-          end
-
-          register_instance_option :bootstrap_wysihtml5_js_location do
-            '/assets/bootstrap-wysihtml5.js'
+          [:bootstrap_wysihtml5, :bootstrap_wysihtml5_config_options, :bootstrap_wysihtml5_css_location, :bootstrap_wysihtml5_js_location].each do |key|
+            register_deprecated_instance_option key do
+              raise "The 'field(:foo){ bootstrap_wysihtml5 true }' style DSL is deprecated. Please use 'field :foo, :wysihtml5' instead."
+            end
           end
 
           register_instance_option :html_attributes do
--- spec/integration/config/edit/rails_admin_config_edit_spec.rb
+++ spec/integration/config/edit/rails_admin_config_edit_spec.rb
@@ -815,18 +815,10 @@ describe "RailsAdmin Config DSL Edit Section" do
   end
 
   describe "CKEditor Support" do
-
-    it "starts with CKEditor disabled" do
-       field = RailsAdmin::config("Draft").edit.fields.find{|f| f.name == :notes}
-       expect(field.ckeditor).to be_false
-    end
-
     it "adds Javascript to enable CKEditor" do
       RailsAdmin.config Draft do
         edit do
-          field :notes do
-            ckeditor true
-          end
+          field :notes, :ck_editor
         end
       end
       visit new_path(:model_name => "draft")
@@ -835,18 +827,10 @@ describe "RailsAdmin Config DSL Edit Section" do
   end
 
   describe "CodeMirror Support" do
-
-    it "starts with CodeMirror disabled" do
-       field = RailsAdmin::config("Draft").edit.fields.find{|f| f.name == :notes}
-       expect(field.codemirror).to be_false
-    end
-
     it "adds Javascript to enable CodeMirror" do
       RailsAdmin.config Draft do
         edit do
-          field :notes do
-            codemirror true
-          end
+          field :notes, :code_mirror
         end
       end
       visit new_path(:model_name => "draft")
@@ -855,18 +839,10 @@ describe "RailsAdmin Config DSL Edit Section" do
   end
 
   describe "bootstrap_wysihtml5 Support" do
-
-    it "starts with bootstrap_wysihtml5 disabled" do
-       field = RailsAdmin::config("Draft").edit.fields.find{|f| f.name == :notes}
-       expect(field.bootstrap_wysihtml5).to be_false
-    end
-
     it "adds Javascript to enable bootstrap_wysihtml5" do
       RailsAdmin.config Draft do
         edit do
-          field :notes do
-            bootstrap_wysihtml5 true
-          end
+          field :notes, :wysihtml5
         end
       end
       visit new_path(:model_name => "draft")
@@ -876,11 +852,10 @@ describe "RailsAdmin Config DSL Edit Section" do
     it "should include custom wysihtml5 configuration" do
       RailsAdmin.config Draft do
         edit do
-          field :notes do
-            bootstrap_wysihtml5 true
-            bootstrap_wysihtml5_config_options :image => false
-            bootstrap_wysihtml5_css_location 'stub_css.css'
-            bootstrap_wysihtml5_js_location 'stub_js.js'
+          field :notes, :wysihtml5 do
+            config_options :image => false
+            css_location 'stub_css.css'
+            js_location 'stub_js.js'
           end
         end
       end
