73e8b9acdd2219a1de595af85d76976198de788c
Erik Michaels-Ober <sferik@gmail.com>
2013-06-28 03:55:39 +0200

    Bump version to 0.5.0

--- lib/rails_admin/version.rb
+++ lib/rails_admin/version.rb
@@ -1,8 +1,8 @@
 module RailsAdmin
   class Version
     MAJOR = 0 unless defined? MAJOR
-    MINOR = 4 unless defined? MINOR
-    PATCH = 9 unless defined? PATCH
+    MINOR = 5 unless defined? MINOR
+    PATCH = 0 unless defined? PATCH
     PRE = nil unless defined? PRE
 
     class << self
