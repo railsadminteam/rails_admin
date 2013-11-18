d69516fd19a4b68e32b64de29709616ede65ff33
ideadapt <kunz@ideadapt.net>
2013-11-18 16:21:47 +0100

    reestablish original indentations

--- lib/rails_admin/adapters/active_record.rb
+++ lib/rails_admin/adapters/active_record.rb
@@ -64,18 +64,18 @@ module RailsAdmin
       def associations
         model.reflect_on_all_associations.map do |association|
           {
-              :name => association.name.to_sym,
-              :pretty_name => association.name.to_s.tr('_', ' ').capitalize,
-              :type => association.macro,
-              :model_proc => Proc.new { association_model_lookup(association) },
-              :primary_key_proc => Proc.new { association_primary_key_lookup(association) },
-              :foreign_key => association_foreign_key_lookup(association),
-              :foreign_type => association_foreign_type_lookup(association),
-              :as => association_as_lookup(association),
-              :polymorphic => association_polymorphic_lookup(association),
-              :inverse_of => association_inverse_of_lookup(association),
-              :read_only => association_read_only_lookup(association),
-              :nested_form => association_nested_attributes_options_lookup(association)
+            :name => association.name.to_sym,
+            :pretty_name => association.name.to_s.tr('_', ' ').capitalize,
+            :type => association.macro,
+            :model_proc => Proc.new { association_model_lookup(association) },
+            :primary_key_proc => Proc.new { association_primary_key_lookup(association) },
+            :foreign_key => association_foreign_key_lookup(association),
+            :foreign_type => association_foreign_type_lookup(association),
+            :as => association_as_lookup(association),
+            :polymorphic => association_polymorphic_lookup(association),
+            :inverse_of => association_inverse_of_lookup(association),
+            :read_only => association_read_only_lookup(association),
+            :nested_form => association_nested_attributes_options_lookup(association)
           }
         end
       end
@@ -86,11 +86,11 @@ module RailsAdmin
         end
         columns.map do |property|
           {
-              :name => property.name.to_sym,
-              :pretty_name => property.name.to_s.tr('_', ' ').capitalize,
-              :length => property.limit,
-              :nullable? => property.null,
-              :serial? => property.primary,
+            :name => property.name.to_sym,
+            :pretty_name => property.name.to_s.tr('_', ' ').capitalize,
+            :length => property.limit,
+            :nullable? => property.null,
+            :serial? => property.primary,
           }.merge(type_lookup(property))
         end
       end
@@ -188,79 +188,79 @@ module RailsAdmin
 
         # now we go type specific
         case type
-          when :boolean
-            return ["(#{column} IS NULL OR #{column} = ?)", false] if ['false', 'f', '0'].include?(value)
-            return ["(#{column} = ?)", true] if ['true', 't', '1'].include?(value)
-          when :integer, :decimal, :float
-            case value
-              when Array then
-                val, range_begin, range_end = *value.map do |v|
-                  if (v.to_i.to_s == v || v.to_f.to_s == v)
-                    type == :integer ? v.to_i : v.to_f
-                  else
-                    nil
-                  end
-                end
-                case operator
-                  when 'between'
-                    if range_begin && range_end
-                      ["(#{column} BETWEEN ? AND ?)", range_begin, range_end]
-                    elsif range_begin
-                      ["(#{column} >= ?)", range_begin]
-                    elsif range_end
-                      ["(#{column} <= ?)", range_end]
-                    end
-                  else
-                    ["(#{column} = ?)", val] if val
-                end
-              else
-                if value.to_i.to_s == value || value.to_f.to_s == value
-                  type == :integer ? ["(#{column} = ?)", value.to_i] : ["(#{column} = ?)", value.to_f]
+        when :boolean
+          return ["(#{column} IS NULL OR #{column} = ?)", false] if ['false', 'f', '0'].include?(value)
+          return ["(#{column} = ?)", true] if ['true', 't', '1'].include?(value)
+        when :integer, :decimal, :float
+          case value
+            when Array then
+              val, range_begin, range_end = *value.map do |v|
+                if (v.to_i.to_s == v || v.to_f.to_s == v)
+                  type == :integer ? v.to_i : v.to_f
                 else
                   nil
                 end
-            end
-          when :belongs_to_association
-            return if value.blank?
-            ["(#{column} = ?)", value.to_i] if value.to_i.to_s == value
-          when :string, :text
-            return if value.blank?
-            value = case operator
-                      when 'default', 'like'
-                        "%#{value.downcase}%"
-                      when 'starts_with'
-                        "#{value.downcase}%"
-                      when 'ends_with'
-                        "%#{value.downcase}"
-                      when 'is', '='
-                        "#{value.downcase}"
-                      else
-                        return
-                    end
-            ["(LOWER(#{column}) #{like_operator} ?)", value]
-          when :date
-            start_date, end_date = get_filtering_duration(operator, value)
+              end
+              case operator
+                when 'between'
+                  if range_begin && range_end
+                    ["(#{column} BETWEEN ? AND ?)", range_begin, range_end]
+                  elsif range_begin
+                    ["(#{column} >= ?)", range_begin]
+                  elsif range_end
+                    ["(#{column} <= ?)", range_end]
+                  end
+                else
+                  ["(#{column} = ?)", val] if val
+              end
+            else
+              if value.to_i.to_s == value || value.to_f.to_s == value
+                type == :integer ? ["(#{column} = ?)", value.to_i] : ["(#{column} = ?)", value.to_f]
+              else
+                nil
+              end
+          end
+        when :belongs_to_association
+          return if value.blank?
+          ["(#{column} = ?)", value.to_i] if value.to_i.to_s == value
+        when :string, :text
+          return if value.blank?
+          value = case operator
+                    when 'default', 'like'
+                      "%#{value.downcase}%"
+                    when 'starts_with'
+                      "#{value.downcase}%"
+                    when 'ends_with'
+                      "%#{value.downcase}"
+                    when 'is', '='
+                      "#{value.downcase}"
+                    else
+                      return
+                  end
+          ["(LOWER(#{column}) #{like_operator} ?)", value]
+        when :date
+          start_date, end_date = get_filtering_duration(operator, value)
 
-            if start_date && end_date
-              ["(#{column} BETWEEN ? AND ?)", start_date, end_date]
-            elsif start_date
-              ["(#{column} >= ?)", start_date]
-            elsif end_date
-              ["(#{column} <= ?)", end_date]
-            end
-          when :datetime, :timestamp
-            start_date, end_date = get_filtering_duration(operator, value)
+          if start_date && end_date
+            ["(#{column} BETWEEN ? AND ?)", start_date, end_date]
+          elsif start_date
+            ["(#{column} >= ?)", start_date]
+          elsif end_date
+            ["(#{column} <= ?)", end_date]
+          end
+        when :datetime, :timestamp
+          start_date, end_date = get_filtering_duration(operator, value)
 
-            if start_date && end_date
-              ["(#{column} BETWEEN ? AND ?)", start_date.to_time.beginning_of_day, end_date.to_time.end_of_day]
-            elsif start_date
-              ["(#{column} >= ?)", start_date.to_time.beginning_of_day]
-            elsif end_date
-              ["(#{column} <= ?)", end_date.to_time.end_of_day]
-            end
-          when :enum
-            return if value.blank?
-            ["(#{column} IN (?))", Array.wrap(value)]
+          if start_date && end_date
+            ["(#{column} BETWEEN ? AND ?)", start_date.to_time.beginning_of_day, end_date.to_time.end_of_day]
+          elsif start_date
+            ["(#{column} >= ?)", start_date.to_time.beginning_of_day]
+          elsif end_date
+            ["(#{column} <= ?)", end_date.to_time.end_of_day]
+          end
+        when :enum
+          return if value.blank?
+          ["(#{column} IN (?))", Array.wrap(value)]
         end
       end
 
