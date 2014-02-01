c920dc8cf2f6d56a2302900f0c2f55244f3a209f
linchus <linchus@gmail.com>
2014-02-01 16:17:28 +0400

    change parent class and add test

--- lib/rails_admin/config/fields/types/json.rb
+++ lib/rails_admin/config/fields/types/json.rb
@@ -1,26 +1,15 @@
-require 'rails_admin/config/fields/base'
+require 'rails_admin/config/fields/types/text'
 
 module RailsAdmin
   module Config
     module Fields
       module Types
-        class Json < RailsAdmin::Config::Fields::Base
+        class Json < RailsAdmin::Config::Fields::Types::Text
           # Register field type for the type loader
           RailsAdmin::Config::Fields::Types::register(self)
 
-          register_instance_option :html_attributes do
-            {
-                :cols => '48',
-                :rows => '3'
-            }
-          end
-
-          register_instance_option :partial do
-            :form_text
-          end
-
           register_instance_option :formatted_value do
-            value.present? ? value.to_json : nil
+            value.present? ? JSON.pretty_generate(value) : nil
           end
 
           def parse_input(params)
