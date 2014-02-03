e2a9f558ea3f6f1864732973153b9d60a34294c9
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-02-03 18:31:56 +0400

    Support for paper_trail custom version classes
    Conflicts:
    	lib/rails_admin/extensions/paper_trail/auditing_adapter.rb

--- lib/rails_admin/extensions/paper_trail/auditing_adapter.rb
+++ lib/rails_admin/extensions/paper_trail/auditing_adapter.rb
@@ -48,7 +48,7 @@ module RailsAdmin
           end
 
           begin
-            @version_class = version_class.to_s.constantize 
+            @version_class = version_class.to_s.constantize
           rescue NameError => e
             raise "Please set up Papertrail's version model explicitely. Ex: config.audit_with :paper_trail, 'User', 'PaperTrail::Version'"
           end
@@ -86,12 +86,23 @@ module RailsAdmin
             sort = :created_at
             sort_reverse = "true"
           end
-          versions = @version_class.where :item_type => model.model.name
+
+          model_name = model.model.name
+
+          versions = version_class_for(model_name).where :item_type => model.model.name
           versions = versions.where :item_id => object.id if object
           versions = versions.where("event LIKE ?", "%#{query}%") if query.present?
           versions = versions.order(sort_reverse == "true" ? "#{sort} DESC" : sort)
           versions = all ? versions : versions.send(Kaminari.config.page_method_name, page.presence || "1").per(per_page)
           versions.map{|version| VersionProxy.new(version, @user_class)}
+        end
+
+        def version_class_for(model_name)
+          klass = model_name.constantize
+            .try(:version_class_name)
+            .try(:constantize)
+
+          klass || @version_class
         end
       end
     end
