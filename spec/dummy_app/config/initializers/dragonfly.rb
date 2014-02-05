0e5ee602fb3e8587347453dade975905d2b7bae4
M.Shibuya <mit.shibuya@gmail.com>
2014-02-05 16:37:43 +0900

    Fix rubocop offence

--- spec/dummy_app/config/initializers/dragonfly.rb
+++ spec/dummy_app/config/initializers/dragonfly.rb
@@ -5,13 +5,13 @@ Dragonfly.app.configure do
   plugin :imagemagick
 
   protect_from_dos_attacks true
-  secret "904547b2be647f0e11a76933b3220d6bd2fb83f380ac760125e4daa3b9504b4e"
+  secret '904547b2be647f0e11a76933b3220d6bd2fb83f380ac760125e4daa3b9504b4e'
 
-  url_format "/media/:job/:name"
+  url_format '/media/:job/:name'
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  datastore(:file,
+            root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+            server_root: Rails.root.join('public'))
 end
 
 # Logger
