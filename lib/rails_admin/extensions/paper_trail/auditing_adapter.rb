9692ca1de30eec320320ff860b9aa9e8e6600797
Andrey Peresleguine <peresleguine@gmail.com>
2014-03-13 19:54:08 +0400

    changed scope to find the user

--- lib/rails_admin/extensions/paper_trail/auditing_adapter.rb
+++ lib/rails_admin/extensions/paper_trail/auditing_adapter.rb
@@ -21,7 +21,7 @@ module RailsAdmin
         end
 
         def username
-          @user_class.find_by_id(@version.whodunnit).try(:email) || @version.whodunnit
+          @user_class.find(@version.whodunnit).try(:email) rescue nil || @version.whodunnit
         end
 
         def item
