ce942b6299d171469bf54478730b0cbbd38dc0c2
M.Shibuya <mit.shibuya@gmail.com>
2014-03-13 15:21:35 +0900

    Failing spec for #1905

--- spec/helpers/rails_admin/form_builder_spec.rb
+++ spec/helpers/rails_admin/form_builder_spec.rb
@@ -14,4 +14,24 @@ describe 'RailsAdmin::FormBuilder' do
       expect(@builder.generate(action: :create, model_config: RailsAdmin.config(Player))).to have_css('.control-group.error')
     end
   end
+
+  describe '#object_infos' do
+    before do
+      allow(helper).to receive(:authorized?).and_return(true)
+      @object = Fan.create!(name: 'foo')
+      @builder = RailsAdmin::FormBuilder.new(:fan, @object, helper, {}, nil)
+    end
+
+    it 'returns a tag with infos' do
+      expect(@builder.object_infos).to eql '<span style="display:none" class="object-infos" data-model-label="Fan" data-object-label="foo"></span>'
+    end
+
+    context 'when object_label\'s type is symbol' do
+      before { @object.name = :foo }
+
+      it 'does not break' do
+        expect(@builder.object_infos).to eql '<span style="display:none" class="object-infos" data-model-label="Fan" data-object-label="foo"></span>'
+      end
+    end
+  end
 end
