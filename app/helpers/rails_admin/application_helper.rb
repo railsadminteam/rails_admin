2cc0ab2ee7b9355fa72932c32909266d9f64b117
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2014-04-15 15:12:04 +0800

    Add instance option 'confirmation_required' that will require that an option shows a confirmation prompt

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -117,9 +117,11 @@ module RailsAdmin
       actions = actions(parent, abstract_model, object).select { |a| a.http_methods.include?(:get) }
       actions.collect do |action|
         wording = wording_for(:menu, action)
+        confirm = action.confirmation_required ? "data-confirm=\"#{t("admin.actions.confirm_prompt")}\"" : ""
+        href = url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))
         %(
           <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)}">
-            <a class="#{action.pjax? ? 'pjax' : ''}" href="#{url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))}">
+            <a class="#{action.pjax? ? 'pjax' : ''}" #{confirm} href="#{href}">
               <i class="#{action.link_icon}"></i>
               <span#{only_icon ? " style='display:none'" : ''}>#{wording}</span>
             </a>
