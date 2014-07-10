efbc49cc600b417b5bff671cb1151c68fd711462
Ace Subido and Terence Ponce <dev+ace+terence@aelogica.com>
2014-07-10 14:08:10 +0800

    [#74753598] ExpressRailsAdmin side navigation implemented in ExpressAdmin dashboard layout

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -58,7 +58,7 @@ module RailsAdmin
         li_stack = navigation nodes_stack, nodes
 
         label = navigation_label || t('admin.misc.navigation')
-        %(<li class='nav-header'>#{label}</li>#{li_stack}) if li_stack.present?
+        %(<h5 class='title'>#{label}</h5>#{li_stack}) if li_stack.present?
       end.join.html_safe
     end
 
