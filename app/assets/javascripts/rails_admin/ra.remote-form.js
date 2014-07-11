8597e1b35d3ae93ac9b70e4b93a6ba9ef3144c92
Ace Subido and Karlo Nicholas Soriano <dev+ace+karlo@aelogica.com>
2014-07-11 09:30:55 +0800

    [#74753598] Foundation modals are now reopening properly

--- app/assets/javascripts/rails_admin/ra.remote-form.js
+++ app/assets/javascripts/rails_admin/ra.remote-form.js
@@ -131,16 +131,17 @@
               ...\
             </div>\
             <div class="modal-footer">\
-              <a href="#" class="button cancel-action">...</a>\
-              <a href="#" class="button save-action">...</a>\
+              <a href="#" class="button gray small radius cancel-action">...</a>\
+              <a href="#" class="button small radius save-action">...</a>\
             </div>\
           </div>')
-          .foundation('reveal', 'open')
-          .on('hidden', function(){
-            widget.dialog.remove();   // We don't want to reuse closed modals
-            widget.dialog = null;
-          });
-        }
+          .foundation('reveal', 'open');
+
+        $(document).on('closed.fndtn.reveal', '[data-reveal]', function(){
+          widget.dialog.remove();   // We don't want to reuse closed modals
+          widget.dialog = null;
+        });
+      }
       return this.dialog;
     }
   });
