ca3087827abc6c2eadc5bcc0bd2470952cdf2a23
Ace Subido and Terence Ponce <dev+ace+terence@aelogica.com>
2014-07-15 10:49:50 +0800

    [#75002932] Create button toolbar instead of tabs in local menus

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -114,12 +114,14 @@ module RailsAdmin
       actions.collect do |action|
         wording = wording_for(:menu, action)
         %(
-          <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)} tab-title">
-            <a href="#{url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))}">
-              <i class="icon #{action.link_icon}"></i>
-              <span#{only_icon ? " style='display:none'" : ''}>#{wording}</span>
-            </a>
-          </li>
+          <ul class="button-group left">
+            <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)} tab-title">
+              <a class="button small radius" href="#{url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))}">
+                <i class="icon #{action.link_icon}"></i>
+                <span#{only_icon ? " style='display:none'" : ''}>#{wording}</span>
+              </a>
+            </li>
+          </ul>
         )
       end.join.html_safe
     end
--- lib/rails_admin/config/actions/show.rb
+++ lib/rails_admin/config/actions/show.rb
@@ -26,7 +26,7 @@ module RailsAdmin
         end
 
         register_instance_option :link_icon do
-          'ios-android-information'
+          'ion-android-information'
         end
       end
     end
