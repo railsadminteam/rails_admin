d4897ff4038fcb9f866457ce6283e8f3fc3aebc6
Jake Quain <quainjn@gmail.com>
2014-08-29 11:21:02 -0400

    Be able to configure default association limit

--- lib/rails_admin/config.rb
+++ lib/rails_admin/config.rb
@@ -41,6 +41,9 @@ module RailsAdmin
       # been configured
       attr_accessor :default_items_per_page
 
+      # Default association limit
+      attr_accessor :default_associated_collection_limit
+
       attr_reader :default_search_operator
 
       # Configuration option to specify which method names will be searched for
@@ -261,6 +264,7 @@ module RailsAdmin
         @default_hidden_fields[:edit] = [:id, :_id, :created_at, :created_on, :deleted_at, :updated_at, :updated_on, :deleted_on]
         @default_hidden_fields[:show] = [:id, :_id, :created_at, :created_on, :deleted_at, :updated_at, :updated_on, :deleted_on]
         @default_items_per_page = 20
+        @default_associated_collection_limit = 100
         @default_search_operator = 'default'
         @excluded_models = []
         @included_models = []
