09fe1b2597dd8810c165731d8bb395b6c86ecc50
M.Shibuya <mit.shibuya@gmail.com>
2014-10-22 12:47:06 +0900

    Use String#encode instead of iconv

--- lib/rails_admin/support/csv_converter.rb
+++ lib/rails_admin/support/csv_converter.rb
@@ -1,12 +1,10 @@
 # encoding: UTF-8
-require RUBY_VERSION < '1.9' ? 'fastercsv' : 'csv'
+require 'csv'
 
 module RailsAdmin
-  CSV_CLASS = RUBY_VERSION < '1.9' ? ::FasterCSV : ::CSV
-  NON_ASCII_ENCODINGS = /(UTF\-16)|(UTF\-32)|(EUC\-JP)|(ISO\-2022\-JP)|(Shift_JIS)|(Big5\-HKSCS)|(UTF\-7)/
-  UTF8_ENCODINGS = [nil, '', 'utf8', 'utf-8', 'unicode', 'UTF8', 'UTF-8', 'UNICODE']
-
   class CSVConverter
+    UTF8_ENCODINGS = [nil, '', 'utf8', 'utf-8', 'unicode', 'UTF8', 'UTF-8', 'UNICODE']
+    TARGET_ENCODINGS = %w(UTF-8 UTF-16LE UTF-16BE UTF-32LE UTF-32BE UTF-7 ISO-8859-1 ISO-8859-15 IBM850 MacRoman Windows-1252 ISO-8859-3 IBM852 ISO-8859-2 Windows-1250 IBM855 ISO-8859-5 KOI8-R MacCyrillic Windows-1251 IBM866 GB2312 GBK GB18030 Big5 Big5-HKSCS EUC-TW EUC-JP ISO-2022-JP Shift_JIS EUC-KR)
     def initialize(objects = [], schema = {})
       return self if (@objects = objects).blank?
 
@@ -37,33 +35,23 @@ module RailsAdmin
 
     def to_csv(options = {})
       # encoding shenanigans first
-      @encoding_from = UTF8_ENCODINGS.include?(@abstract_model.encoding) ? 'UTF-8' : @abstract_model.encoding
-
-      @encoding_to = options[:encoding_to].presence || @encoding_from
+      @encoding_from = Encoding.find(UTF8_ENCODINGS.include?(@abstract_model.encoding) ? 'UTF-8' : @abstract_model.encoding)
+      @encoding_to = Encoding.find(options[:encoding_to].presence || @encoding_from)
 
       csv_string = generate_csv_string(options)
 
+      if @encoding_to != @encoding_from
+        csv_string = csv_string.encode(@encoding_to, @encoding_from, invalid: :replace, undef: :replace, replace: '?')
+      end
       # Add a BOM for utf8 encodings, helps with utf8 auto-detect for some versions of Excel.
       # Don't add if utf8 but user don't want to touch input encoding:
       # If user chooses utf8, they will open it in utf8 and BOM will disappear at reading.
       # But that way "English" users who don't bother and chooses to let utf8 by default won't get BOM added
       # and will not see it if Excel opens the file with a different encoding.
-      if options[:encoding_to] == 'UTF-8'
-        csv_string = csv_string.force_encoding('UTF-8') if csv_string.respond_to?(:force_encoding)
+      if options[:encoding_to].present? && @encoding_to == Encoding::UTF_8
         csv_string = "\xEF\xBB\xBF#{csv_string}"
       end
-      # global conversion for non ASCII encodings
-      if @encoding_to =~ NON_ASCII_ENCODINGS && @encoding_to != @encoding_from
-        require 'iconv'
-        begin
-          if @iconv = Iconv.new("#{@encoding_to}//TRANSLIT//IGNORE", @encoding_from)
-            csv_string = @iconv.iconv(csv_string) rescue csv_string
-          end
-        rescue
-          Rails.logger.error("Iconv cannot convert to #{@encoding_to}: #{$ERROR_INFO}\nNo conversion will take place")
-        end
-      end
-      [!options[:skip_header], @encoding_to, csv_string]
+      [!options[:skip_header], @encoding_to.to_s, csv_string]
     end
 
   private
@@ -78,7 +66,7 @@ module RailsAdmin
 
     def generate_csv_string(options)
       generator_options = (options[:generator] || {}).symbolize_keys.delete_if { |_, value| value.blank? }
-      CSV_CLASS.generate(generator_options) do |csv|
+      CSV.generate(generator_options) do |csv|
         csv << generate_csv_header unless options[:skip_header]
 
         method = @objects.respond_to?(:find_each) ? :find_each : :each
@@ -90,39 +78,23 @@ module RailsAdmin
 
     def generate_csv_header
       @fields.collect do |field|
-        output(::I18n.t('admin.export.csv.header_for_root_methods', name: field.label, model: @abstract_model.pretty_name))
+        ::I18n.t('admin.export.csv.header_for_root_methods', name: field.label, model: @abstract_model.pretty_name)
       end +
       @associations.flat_map do |_association_name, option_hash|
         option_hash[:fields].collect do |field|
-          output(::I18n.t('admin.export.csv.header_for_association_methods', name: field.label, association: option_hash[:association].label))
+          ::I18n.t('admin.export.csv.header_for_association_methods', name: field.label, association: option_hash[:association].label)
         end
       end
     end
 
     def generate_csv_row(object)
       @fields.collect do |field|
-        output(field.with(object: object).export_value)
+        field.with(object: object).export_value
       end +
       @associations.flat_map do |association_name, option_hash|
         associated_objects = [object.send(association_name)].flatten.compact
         option_hash[:fields].collect do |field|
-          output(associated_objects.collect { |ao| field.with(object: ao).export_value.presence || @empty }.join(','))
-        end
-      end
-    end
-
-    def output(str)
-      # Can't use the CSV generator with encodings that are not supersets of ASCII-7
-      return str.to_s if @encoding_to =~ NON_ASCII_ENCODINGS
-
-      # Convert piece by piece
-      if @iconv
-        @iconv.iconv(str.to_s) rescue str.to_s
-      else
-        if @encoding_from != @encoding_to
-          str.to_s.encode(@encoding_to, @encoding_from, invalid: :replace, undef: :replace, replace: '?')
-        else
-          str.to_s
+          associated_objects.collect { |ao| field.with(object: ao).export_value.presence || @empty }.join(',')
         end
       end
     end
--- spec/rails_admin/support/csv_converter_spec.rb
+++ spec/rails_admin/support/csv_converter_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe RailsAdmin::CSVConverter do
@@ -15,7 +17,6 @@ describe RailsAdmin::CSVConverter do
     expect(RailsAdmin::CSVConverter.new(objects, schema).to_csv({})[2]).to match(/Number,Name/)
   end
 
-
   describe '#to_csv' do
     before do
       RailsAdmin.config(Player) do
@@ -24,33 +25,44 @@ describe RailsAdmin::CSVConverter do
           field :name
         end
       end
-
-      FactoryGirl.create :player, name: 'なまえ'
     end
 
-    let(:objects) { Player.all }
+    let(:objects) { FactoryGirl.create_list :player, 1, number: 1, name: 'なまえ' }
     let(:schema) { {only: [:number, :name]} }
 
-    subject { RailsAdmin::CSVConverter.new(objects, schema).to_csv({encoding_to: encoding}) }
+    subject { RailsAdmin::CSVConverter.new(objects, schema).to_csv(encoding_to: encoding) }
 
-    context 'encoding to UTF-8' do
+    context 'when encoding to UTF-8' do
       let(:encoding) { 'UTF-8' }
 
-      it 'should export to UTR-8 with BOM' do
+      it 'exports to UTR-8 with BOM' do
         expect(subject[1]).to eq 'UTF-8'
         expect(subject[2].encoding).to eq Encoding::UTF_8
-        expect(subject[2]).to match(/\A\xEF\xBB\xBF/)  # have BOM
+        expect(subject[2].unpack('H*').first).
+          to eq 'efbbbf4e756d6265722c4e616d650a312ce381aae381bee381880a'  # have BOM
       end
     end
 
-    context 'encoding to Shift_JIS' do
+    context 'when encoding to Shift_JIS' do
       let(:encoding) { 'Shift_JIS' }
 
-      it 'should export to Shift_JIS' do
+      it 'exports to Shift_JIS' do
         expect(subject[1]).to eq 'Shift_JIS'
         expect(subject[2].encoding).to eq Encoding::Shift_JIS
+        expect(subject[2].unpack('H*').first).
+          to eq '4e756d6265722c4e616d650a312c82c882dc82a60a'
       end
     end
 
+    context 'when encoding to UTF-16(ASCII-incompatible)' do
+      let(:encoding) { 'UTF-16' }
+
+      it 'encodes to expected byte sequence' do
+        expect(subject[1]).to eq 'UTF-16'
+        expect(subject[2].encoding).to eq Encoding::UTF_16
+        expect(subject[2].unpack('H*').first).
+          to eq 'feff004e0075006d006200650072002c004e0061006d0065000a0031002c306a307e3048000a'
+      end
+    end
   end
 end
