444362e504734156318264aeabb17068f8902fa3
M.Shibuya <mit.shibuya@gmail.com>
2014-12-26 17:59:56 +0900

    Fix mongoid build error

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -30,6 +30,6 @@ module DummyApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true if Rails.version >= '4.2'
+    config.active_record.raise_in_transactional_callbacks = true if Rails.version >= '4.2' && CI_ORM == :active_record
   end
 end
