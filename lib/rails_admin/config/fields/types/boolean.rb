78d71104675436150dd74312a6b273078a25d805
M.Shibuya <mit.shibuya@gmail.com>
2014-12-29 11:52:22 +0900

    Replace dash to entity reference
    Pre-2.0 Rubies do not use UTF-8 source encoding by default

--- lib/rails_admin/config/fields/types/boolean.rb
+++ lib/rails_admin/config/fields/types/boolean.rb
@@ -13,7 +13,7 @@ module RailsAdmin
           register_instance_option :pretty_value do
             case value
             when nil
-              %(<span class='label label-default'>‒</span>)
+              %(<span class='label label-default'>&#x2012;</span>)
             when false
               %(<span class='label label-danger'>&#x2718;</span>)
             when true
