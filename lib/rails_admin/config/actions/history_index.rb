3735f0553a84cc9def6d17791d3ed0a8fd42ea99
Rikki Pitt <rikkipitt@gmail.com>
2015-03-03 14:05:49 +0000

    Add rollback/revert feature to paper_trail history, solves #1857

--- lib/rails_admin/config/actions/history_index.rb
+++ lib/rails_admin/config/actions/history_index.rb
@@ -12,6 +12,10 @@ module RailsAdmin
           true
         end
 
+        register_instance_option :http_methods do
+          [:get, :put] # NEW / ROLLBACK
+        end
+
         register_instance_option :route_fragment do
           'history'
         end
@@ -20,8 +24,42 @@ module RailsAdmin
           proc do
             @general = true
             @history = @auditing_adapter && @auditing_adapter.listing_for_model(@abstract_model, params[:query], params[:sort], params[:sort_reverse], params[:all], params[:page]) || []
+            @version = PaperTrail::Version.find(params[:version_id]) if params[:version_id] rescue false
 
-            render @action.template_name
+            if request.get? # SHOW
+
+              if @version
+                render partial: 'version', layout: false
+              else
+                render @action.template_name
+              end
+
+            elsif request.put? # ROLLBACK
+              begin
+                if @version.event == "create"
+                  @record = @version.item_type.constantize.find(@version.item_id)
+                  @result = @record.destroy
+                else
+                  @record = @version.reify
+                  @result = @record.save
+                end
+
+                if @result
+                  if @version.event == "create"
+                    flash[:success] = "Rolled back newly-created record by destroying it."
+                  else
+                    flash[:success] = "Rolled back changes to this record."
+                  end
+                else
+                  flash[:error] = "Couldn't rollback. Sorry."
+                end
+              rescue ActiveRecord::RecordNotFound
+                flash[:error] = "Version does not exist."
+                redirect_to :action => :history_index
+              end
+
+              redirect_to :action => :history_index
+            end
           end
         end
 
