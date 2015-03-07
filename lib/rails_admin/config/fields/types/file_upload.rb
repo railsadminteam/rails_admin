6a401eefc848a90c4757baa3178f2bae0cb0a4c3
Ohkubo KOHEI <ohkubo@magician.jp>
2015-03-08 05:31:45 +0900

    fix target typo 'blank' to '_blank'

--- lib/rails_admin/config/fields/types/file_upload.rb
+++ lib/rails_admin/config/fields/types/file_upload.rb
@@ -35,7 +35,7 @@ module RailsAdmin
                 thumb_url = resource_url(thumb_method)
                 url != thumb_url ? v.link_to(v.image_tag(thumb_url, class: 'img-polaroid'), url, target: 'blank') : v.image_tag(thumb_url)
               else
-                v.link_to(nil, url, target: 'blank')
+                v.link_to(nil, url, target: '_blank')
               end
             end
           end
