3503f5d5c2d42776822347530d32c78c1b9d4fd6
akm <akm2000@gmail.com>
2015-04-20 22:29:24 +0900

    return 404 response when objects by bulk_ids are already deleted.
    Without this fix, rails_admin will find without condition of id,
    So ALL record of the model will be DELETED!!!

--- lib/rails_admin/config/actions/bulk_delete.rb
+++ lib/rails_admin/config/actions/bulk_delete.rb
@@ -25,22 +25,25 @@ module RailsAdmin
               end
 
             elsif request.delete? # BULK DESTROY
-
               @objects = list_entries(@model_config, :destroy)
-              processed_objects = @abstract_model.destroy(@objects)
+              if @objects.blank?
+                msg = t('admin.flash.error', name: pluralize(0, @model_config.label), action: t('admin.actions.delete.done'))
+                render text: msg, status: :not_found
+              else
+                processed_objects = @abstract_model.destroy(@objects)
 
-              destroyed = processed_objects.select(&:destroyed?)
-              not_destroyed = processed_objects - destroyed
+                destroyed = processed_objects.select(&:destroyed?)
+                not_destroyed = processed_objects - destroyed
 
-              destroyed.each do |object|
-                @auditing_adapter && @auditing_adapter.delete_object(object, @abstract_model, _current_user)
+                destroyed.each do |object|
+                  @auditing_adapter && @auditing_adapter.delete_object(object, @abstract_model, _current_user)
+                end
+
+                flash[:success] = t('admin.flash.successful', name: pluralize(destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless destroyed.empty?
+                flash[:error] = t('admin.flash.error', name: pluralize(not_destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless not_destroyed.empty?
+
+                redirect_to back_or_index
               end
-
-              flash[:success] = t('admin.flash.successful', name: pluralize(destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless destroyed.empty?
-              flash[:error] = t('admin.flash.error', name: pluralize(not_destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless not_destroyed.empty?
-
-              redirect_to back_or_index
-
             end
           end
         end
