a995c985de86a6194354c10e79c189fdce27adc2
akm <akm2000@gmail.com>
2015-04-20 22:06:16 +0900

    add an example to redirect to index when selected object(s) is/are already deleted.

--- spec/integration/basic/bulk_action/rails_admin_basic_bulk_action_spec.rb
+++ spec/integration/basic/bulk_action/rails_admin_basic_bulk_action_spec.rb
@@ -12,6 +12,22 @@ describe 'RailsAdmin Basic Bulk Action', type: :request do
       post(bulk_action_path(bulk_action: 'bulk_delete', model_name: 'player', bulk_ids: @players.collect(&:id)))
       @players.each { |player| expect(response.body).to include(player.name) }
     end
+
+    it 'redirects to list when all of the records is already deleted' do
+      expect(Player.count).to eq @players.length
+      player_ids = [@players.first.id]
+      @players.first.destroy # delete selected object before send request to show bulk_delete confirmation.
+
+      post(bulk_action_path(bulk_action: 'bulk_delete', model_name: 'player', bulk_ids: player_ids))
+      expect(response.response_code).to eq 302
+
+      index_url = response.headers['Location']
+      expect(URI.parse(index_url).path).to eq(index_path(model_name: 'player'))
+      visit index_url
+
+      is_expected.to have_no_content(@players.first.name)
+      is_expected.to have_content(@players.last.name)
+    end
   end
 
   describe 'bulk_export' do
