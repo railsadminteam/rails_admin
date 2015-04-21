523a3b7a02dc2898ca18b9c050879a74ed2b7b71
akm <akm2000@gmail.com>
2015-04-21 14:39:45 +0900

    fix the offence by more than 3 levels of block nesting
    and remove duplicated code

--- lib/rails_admin/config/actions/bulk_delete.rb
+++ lib/rails_admin/config/actions/bulk_delete.rb
@@ -26,30 +26,30 @@ module RailsAdmin
 
             elsif request.delete? # BULK DESTROY
 
-              if params[:bulk_ids].blank?
-                msg = t('admin.flash.error', name: pluralize(0, @model_config.label), action: t('admin.actions.delete.done'))
-                render text: msg, status: :not_found
-              else
-                @objects = list_entries(@model_config, :destroy)
-                if @objects.blank?
-                  msg = t('admin.flash.error', name: pluralize(0, @model_config.label), action: t('admin.actions.delete.done'))
-                  render text: msg, status: :not_found
-                else
-                  processed_objects = @abstract_model.destroy(@objects)
+              destroyed = nil
+              not_destroyed = nil
 
+              unless params[:bulk_ids].blank?
+                @objects = list_entries(@model_config, :destroy)
+                unless @objects.blank?
+                  processed_objects = @abstract_model.destroy(@objects)
                   destroyed = processed_objects.select(&:destroyed?)
                   not_destroyed = processed_objects - destroyed
-
                   destroyed.each do |object|
                     @auditing_adapter && @auditing_adapter.delete_object(object, @abstract_model, _current_user)
                   end
-
-                  flash[:success] = t('admin.flash.successful', name: pluralize(destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless destroyed.empty?
-                  flash[:error] = t('admin.flash.error', name: pluralize(not_destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless not_destroyed.empty?
-
-                  redirect_to back_or_index
                 end
               end
+
+              if destroyed.nil?
+                msg = t('admin.flash.error', name: pluralize(0, @model_config.label), action: t('admin.actions.delete.done'))
+                render text: msg, status: :not_found
+              else
+                flash[:success] = t('admin.flash.successful', name: pluralize(destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless destroyed.empty?
+                flash[:error] = t('admin.flash.error', name: pluralize(not_destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless not_destroyed.empty?
+                redirect_to back_or_index
+              end
+
             end
           end
         end
