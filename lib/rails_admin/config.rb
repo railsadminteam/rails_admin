4ce5b7a962b0fd9d1487e09828f26af2245839f6
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-07-28 12:57:05 +0200

    Exclude models without database (#2366)

--- lib/rails_admin/config.rb
+++ lib/rails_admin/config.rb
@@ -192,7 +192,9 @@ module RailsAdmin
       def models_pool
         excluded = (excluded_models.collect(&:to_s) + ['RailsAdmin::History'])
 
-        (viable_models - excluded).uniq.sort
+        db_backed_models = viable_models.select { |m| eval "#{m}.try(:columns).try(:any?)"}
+
+        (db_backed_models - excluded).uniq.sort
       end
 
       # Loads a model configuration instance from the registry or registers
