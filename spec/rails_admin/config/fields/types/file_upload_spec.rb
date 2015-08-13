ad5c6e599590cef82fab684fdd56c8f04f6adf7c
M.Shibuya <mit.shibuya@gmail.com>
2015-08-13 19:41:14 +0900

    Browser validation prevented saving of persisted upload fields. Fixes #2376

--- spec/rails_admin/config/fields/types/file_upload_spec.rb
+++ spec/rails_admin/config/fields/types/file_upload_spec.rb
@@ -2,4 +2,26 @@ require 'spec_helper'
 
 describe RailsAdmin::Config::Fields::Types::FileUpload do
   it_behaves_like 'a generic field type', :string_field, :file_upload
+
+  describe '#html_attributes' do
+    context 'when the field is required and value is already set' do
+      before do
+        RailsAdmin.config FieldTest do
+          field :string_field, :file_upload do
+            required true
+          end
+        end
+      end
+
+      let :rails_admin_field do
+        RailsAdmin.config('FieldTest').fields.detect do |f|
+          f.name == :string_field
+        end.with(object: FieldTest.new(string_field: 'dummy.jpg'))
+      end
+
+      it 'does not have a required attribute' do
+        expect(rails_admin_field.html_attributes[:required]).to be_falsy
+      end
+    end
+  end
 end
