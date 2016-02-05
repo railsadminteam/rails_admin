1f9833f455270a01fa3e15e51dd1fa4401b5c58f
Benoit Benezech <benoit.benezech@gmail.com>
2016-02-05 16:31:18 +0100

    adapt old Rails prod server config stuff in dummy app

--- spec/dummy_app/config/environments/production.rb
+++ spec/dummy_app/config/environments/production.rb
@@ -16,7 +16,8 @@ DummyApp::Application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.static_cache_control = 'public, max-age=31536000'
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
