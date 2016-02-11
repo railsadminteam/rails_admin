1364decef75f8e150c2235c1ca1b54c624644f5a
M.Shibuya <mit.shibuya@gmail.com>
2016-02-11 15:50:35 +0900

    Tableless model's attributes need to be manually defined in AR 5

--- spec/orm/active_record.rb
+++ spec/orm/active_record.rb
@@ -28,6 +28,8 @@ class Tableless < ActiveRecord::Base
     end
 
     def column(name, sql_type = nil, default = nil, null = true)
+      define_attribute(name.to_s,
+                       connection.lookup_cast_type(sql_type.to_s)) if ActiveRecord::VERSION::MAJOR >= 5
       columns <<
         if connection.respond_to?(:lookup_cast_type)
           ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, connection.lookup_cast_type(sql_type.to_s), sql_type.to_s, null)
