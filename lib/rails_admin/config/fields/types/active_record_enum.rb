c1d63cc7e29aef01b39f6364a11b24a247dae5fd
Ivan Zotov <ivanzotov@gmail.com>
2016-02-15 14:08:12 +0300

    Support translation for active record enum type

--- lib/rails_admin/config/fields/types/active_record_enum.rb
+++ lib/rails_admin/config/fields/types/active_record_enum.rb
@@ -12,11 +12,11 @@ module RailsAdmin
           end
 
           register_instance_option :enum do
-            abstract_model.model.defined_enums[name.to_s]
+            Hash[defined_enums.map{|k, v| [i18n_method(k), v]}]
           end
 
           register_instance_option :pretty_value do
-            bindings[:object].send(name).presence || ' - '
+            i18n_name || bindings[:object].send(name).presence || ' - '
           end
 
           register_instance_option :multiple? do
@@ -28,11 +28,25 @@ module RailsAdmin
           end
 
           def parse_value(value)
-            value.present? ? enum.invert[value.to_i] : nil
+            value.present? ? defined_enums.invert[value.to_i] : nil
           end
 
           def parse_input(params)
             params[name] = parse_value(params[name]) if params[name]
+          end
+
+          def defined_enums
+            abstract_model.model.defined_enums[name.to_s]
+          end
+
+          def i18n_method(method)
+            _path = "enums.#{abstract_model.model.model_name.i18n_key.to_s}.#{name}.#{method}"
+            I18n.exists?(_path) ? I18n.t(_path) : method
+          end
+
+          def i18n_name
+            _path = "enums.#{abstract_model.model.model_name.i18n_key.to_s}.#{name}.#{bindings[:object].send(name)}"
+            I18n.exists?(_path) ? I18n.t(_path) : nil
           end
         end
       end
