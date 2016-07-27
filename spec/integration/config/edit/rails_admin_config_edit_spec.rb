cd2cb3332c4412e718b818a9d3381df1c0819bac
Adam Rice <arice@Adam-Rices-MacBook-Pro-10.local>
2016-07-27 17:17:37 +1000

    Consistent alignment of opening and closing braces

--- spec/integration/config/edit/rails_admin_config_edit_spec.rb
+++ spec/integration/config/edit/rails_admin_config_edit_spec.rb
@@ -784,7 +784,8 @@ describe 'RailsAdmin Config DSL Edit Section', type: :request do
         expect(find('#field_test_nested_field_tests_attributes_0_title').value).to eq('nested title 1')
         is_expected.not_to have_selector('form .remove_nested_fields')
         expect(find('div#nested_field_tests_fields_blueprint', visible: false)[:'data-blueprint']).to match(
-          /<a[^>]* class="remove_nested_fields"[^>]*>/)
+          /<a[^>]* class="remove_nested_fields"[^>]*>/,
+        )
       end
     end
 
@@ -793,7 +794,8 @@ describe 'RailsAdmin Config DSL Edit Section', type: :request do
         visit new_path(model_name: 'field_test')
         is_expected.not_to have_selector('select#field_test_nested_field_tests_attributes_new_nested_field_tests_field_test_id')
         expect(find('div#nested_field_tests_fields_blueprint', visible: false)[:'data-blueprint']).to match(
-          /<select[^>]* id="field_test_nested_field_tests_attributes_new_nested_field_tests_another_field_test_id"[^>]*>/)
+          /<select[^>]* id="field_test_nested_field_tests_attributes_new_nested_field_tests_another_field_test_id"[^>]*>/,
+        )
       end
 
       it 'hides fields that are deeply nested with inverse_of' do
