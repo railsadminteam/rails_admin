9223a2c006153cd629df4265bf62e358a495e910
YeJun Su <goofansu@users.noreply.github.com>
2017-05-10 20:30:03 +0800

    No need to set timezone value again in Time
    Timezone is already set in Datetime

--- lib/rails_admin/config/fields/types/time.rb
+++ lib/rails_admin/config/fields/types/time.rb
@@ -7,13 +7,6 @@ module RailsAdmin
         class Time < RailsAdmin::Config::Fields::Types::Datetime
           RailsAdmin::Config::Fields::Types.register(self)
 
-          def parse_value(value)
-            parent_value = super(value)
-            return unless parent_value
-            value_with_tz = parent_value.in_time_zone
-            ::DateTime.parse(value_with_tz.strftime('%Y-%m-%d %H:%M:%S'))
-          end
-
           register_instance_option :strftime_format do
             '%H:%M'
           end
