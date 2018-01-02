52a2432884569293a0df7c2e21e12c922570c0e3
Arnvald <arnvald.to@gmail.com>
2018-01-02 14:43:54 +0800

    Rails 5: Corretly populate enum fields in form
    This commit changes an order in which a type of enum field is determined.
    Previously a generic enum field was preferred over ActiveRecord enum field,
    however it caused AR enums to be recognized as generic enums.
    
    Still, it worked correctly until Rails 5, where calling `safe_send` for enum
    field returns a string value (while previously it returned numeric value).
    
    Due to this change, `selected` parameter does not take the correct value and
    on edit form, the enum field is always empty.
    
    It does not cause a problem if enum field is correctly recognized as ActiveRecord enum.
    Therefore, to fix the problem, I swapped the priority of enum fields.

--- lib/rails_admin/config/fields/factories/enum.rb
+++ lib/rails_admin/config/fields/factories/enum.rb
@@ -7,13 +7,13 @@ RailsAdmin::Config::Fields.register_factory do |parent, properties, fields|
   method_name = "#{properties.name}_enum"
 
   # NOTICE: _method_name could be `to_enum` and this method defined in Object.
-  if !Object.respond_to?(method_name) && \
+  if model.respond_to?(:defined_enums) && model.defined_enums[properties.name.to_s]
+    fields << RailsAdmin::Config::Fields::Types::ActiveRecordEnum.new(parent, properties.name, properties)
+    true
+  elsif !Object.respond_to?(method_name) && \
      (model.respond_to?(method_name) || \
          model.method_defined?(method_name))
     fields << RailsAdmin::Config::Fields::Types::Enum.new(parent, properties.name, properties)
-    true
-  elsif model.respond_to?(:defined_enums) && model.defined_enums[properties.name.to_s]
-    fields << RailsAdmin::Config::Fields::Types::ActiveRecordEnum.new(parent, properties.name, properties)
     true
   else
     false
