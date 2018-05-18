578c800660fe861c981aaa5db3c0a2f25919a028
Aashish Kiran <aashish.kiran@gmail.com>
2018-05-18 20:01:22 +0530

    bulk delete shows error flash message on successfull delete

--- lib/rails_admin/config/actions/bulk_delete.rb
+++ lib/rails_admin/config/actions/bulk_delete.rb
@@ -26,26 +26,22 @@ module RailsAdmin
 
             elsif request.delete? # BULK DESTROY
 
-              destroyed = nil
-              not_destroyed = nil
+              destroyed_objects = nil
 
               unless params[:bulk_ids].blank?
                 @objects = list_entries(@model_config, :destroy)
                 unless @objects.blank?
-                  processed_objects = @abstract_model.destroy(@objects)
-                  destroyed = processed_objects.select(&:destroyed?)
-                  not_destroyed = processed_objects - destroyed
-                  destroyed.each do |object|
+                  destroyed_objects = @abstract_model.destroy(@objects)
+                  destroyed_objects.each do |object|
                     @auditing_adapter && @auditing_adapter.delete_object(object, @abstract_model, _current_user)
                   end
                 end
               end
 
-              if destroyed.nil?
+              if destroyed_objects.blank?
                 flash[:error] = t('admin.flash.error', name: pluralize(0, @model_config.label), action: t('admin.actions.delete.done'))
               else
-                flash[:success] = t('admin.flash.successful', name: pluralize(destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless destroyed.empty?
-                flash[:error] = t('admin.flash.error', name: pluralize(not_destroyed.count, @model_config.label), action: t('admin.actions.delete.done')) unless not_destroyed.empty?
+                flash[:success] = t('admin.flash.successful', name: pluralize(destroyed_objects.count, @model_config.label), action: t('admin.actions.delete.done'))
               end
               redirect_to back_or_index
             end
