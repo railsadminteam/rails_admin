59478af9a05c76bdfe35e94e63c60ba89c27a483
M.Shibuya <mit.shibuya@gmail.com>
2018-08-13 18:33:29 +0900

    Do not forcibly add middlewares, let users add them instead if required middlewares are missing
    Refs. #3048, #3006, #2919

--- lib/rails_admin/engine.rb
+++ lib/rails_admin/engine.rb
@@ -24,11 +24,6 @@ module RailsAdmin
     end
 
     initializer 'RailsAdmin setup middlewares' do |app|
-      app.config.session_store :cookie_store
-      app.config.middleware.use ActionDispatch::Cookies
-      app.config.middleware.use ActionDispatch::Flash
-      app.config.middleware.use ActionDispatch::Session::CookieStore, app.config.session_options
-      app.config.middleware.use Rack::MethodOverride
       app.config.middleware.use Rack::Pjax
     end
 
@@ -46,6 +41,25 @@ module RailsAdmin
 
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__), '../tasks/*.rake')].each { |f| load f }
+    end
+
+    initializer 'RailsAdmin check for required middlewares', after: :build_middleware_stack do |app|
+      has_session_store = app.config.middleware.to_a.any? { |m| m.klass.try(:<=, ActionDispatch::Session::AbstractStore) } || ::Rails.version < '5.0'
+      loaded = app.config.middleware.to_a.map(&:name)
+      required = %w(ActionDispatch::Cookies ActionDispatch::Flash Rack::MethodOverride)
+      missing = required - loaded
+      unless missing.empty? && has_session_store
+        configs = missing.map { |m| "config.middleware.use #{m}" }
+        configs << "config.middleware.use #{app.config.session_store.try(:name) || 'ActionDispatch::Session::CookieStore'}, #{app.config.session_options}" unless has_session_store
+        raise <<-EOM
+Required middlewares for RailsAdmin are not added
+To fix tihs, add
+
+  #{configs.join("\n  ")}
+
+to config/application.rb.
+        EOM
+      end
     end
   end
 end
