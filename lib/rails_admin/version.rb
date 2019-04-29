12d009d4acc33f73feb4429630b8c5e04538e185
M.Shibuya <mit.shibuya@gmail.com>
2019-04-29 13:36:25 +0900

    Start development for 2.0.0

--- lib/rails_admin/version.rb
+++ lib/rails_admin/version.rb
@@ -1,9 +1,9 @@
 module RailsAdmin
   class Version
-    MAJOR = 1
-    MINOR = 4
-    PATCH = 2
-    PRE = nil
+    MAJOR = 2
+    MINOR = 0
+    PATCH = 0
+    PRE = 'alpha'
 
     class << self
       # @return [String]
