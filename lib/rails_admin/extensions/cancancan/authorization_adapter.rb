5bebac2488906f3739717108efadedaa091ccaf5
M.Shibuya <mit.shibuya@gmail.com>
2019-07-14 17:58:58 +0900

    Drop support for CanCanCan legacy `can :dashboard` style dashboard ability
    This reverts commit da0584aaa87737427a5d6b9aba332d30c3b4ed45.

--- lib/rails_admin/extensions/cancancan/authorization_adapter.rb
+++ lib/rails_admin/extensions/cancancan/authorization_adapter.rb
@@ -26,12 +26,8 @@ module RailsAdmin
         # instance if it is available.
         def authorize(action, abstract_model = nil, model_object = nil)
           return unless action
-          subject = model_object || abstract_model && abstract_model.model
-          if authorized_for_dashboard_in_legacy_way?(action)
-            subject
-          else
-            @controller.current_ability.authorize!(*resolve_with_compatibility(action, subject))
-          end
+          action, subject = resolve_action_and_subject(action, abstract_model, model_object)
+          @controller.current_ability.authorize!(action, subject)
         end
 
         # This method is called primarily from the view to determine whether the given user
@@ -40,9 +36,8 @@ module RailsAdmin
         # return a boolean whereas +authorize+ will raise an exception when not authorized.
         def authorized?(action, abstract_model = nil, model_object = nil)
           return unless action
-          subject = model_object || abstract_model && abstract_model.model
-          authorized_for_dashboard_in_legacy_way?(action, true) ||
-            @controller.current_ability.can?(*resolve_with_compatibility(action, subject))
+          action, subject = resolve_action_and_subject(action, abstract_model, model_object)
+          @controller.current_ability.can?(action, subject)
         end
 
         # This is called when needing to scope a database query. It is called within the list
@@ -61,18 +56,8 @@ module RailsAdmin
 
       private
 
-        def authorized_for_dashboard_in_legacy_way?(action, silent = false)
-          return false unless action == :dashboard
-          legacy_ability = @controller.current_ability.permissions[:can][:dashboard]
-          if legacy_ability && (legacy_ability.empty? || legacy_ability.all?(&:empty?))
-            ActiveSupport::Deprecation.warn('RailsAdmin CanCanCan Ability with `can :dashboard` is old and support will be removed in the next major release, use `can :read, :dashboard` instead. See https://github.com/sferik/rails_admin/issues/2901') unless silent
-            true
-          else
-            false
-          end
-        end
-
-        def resolve_with_compatibility(action, subject)
+        def resolve_action_and_subject(action, abstract_model, model_object)
+          subject = model_object || abstract_model && abstract_model.model
           if subject
             [action, subject]
           else
