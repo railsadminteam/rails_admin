railsadminteam/rails_admin

commit be7d2f4a3ad1fda788f92a0e0dd4a83b98f141f4

Fix breaking with has_many and custom primary key. Fixes #1878

lib/rails_admin/config/actions/index.rb
@@ -49,7 +49,7 @@ module RailsAdmin
               format.json do
                 output = begin
                   if params[:compact]
-                    primary_key_method = @association ? @association.associated_primary_key : @model_config.abstract_model.primary_key
+                    primary_key_method = @model_config.abstract_model.primary_key
                     label_method = @model_config.object_label_method
                     @objects.collect { |o| {id: o.send(primary_key_method).to_s, label: o.send(label_method).to_s} }
                   else

spec/integration/fields/belongs_to_association_spec.rb
@@ -40,4 +40,23 @@ describe 'BelongsToAssociation field', type: :request do
       is_expected.to have_css("a[href='/admin/team/#{@team.id}']")
     end
   end
+
+  context 'with custom primary_key option', active_record: true do
+    let(:user) { FactoryBot.create :user }
+    let!(:teams) { [FactoryBot.create(:team, manager: user.email), FactoryBot.create(:team)] }
+    before do
+      class ManagedTeam < Team
+        belongs_to :user, foreign_key: :manager, primary_key: :email
+      end
+      RailsAdmin.config.included_models = [ManagedTeam]
+      RailsAdmin.config ManagedTeam do
+        field :user
+      end
+    end
+
+    it 'picks up associated primary key correctly' do
+      visit edit_path(model_name: 'managed_team', id: teams[0].id)
+      expect(page).to have_css("select#managed_team_manager option[value=\"#{user.id}\"]")
+    end
+  end
 end

spec/integration/fields/has_many_association_spec.rb
@@ -248,4 +248,34 @@ describe 'HasManyAssociation field', type: :request do
       is_expected.to have_selector('a.ra-multiselect-item-remove-all')
     end
   end
+
+  context 'with custom primary_key option', active_record: true do
+    let(:user) { FactoryBot.create :user }
+    let!(:teams) { [FactoryBot.create(:team, manager: user.email), FactoryBot.create(:team)] }
+    before do
+      class ManagingUser < User
+        has_many :teams, foreign_key: :manager, primary_key: :email
+      end
+      RailsAdmin.config.included_models = [ManagingUser, Team]
+      RailsAdmin.config ManagingUser do
+        field(:teams) { associated_collection_cache_all false }
+      end
+    end
+
+    it "picks up associated model's primary key correctly" do
+      visit edit_path(model_name: 'managing_user', id: user.id)
+      expect(find("select#managing_user_team_ids option[value=\"#{teams[0].id}\"]")).to have_content teams[0].name
+    end
+
+    it "allows update with fetching associated objects via xhr", js: true do
+      visit edit_path(model_name: 'managing_user', id: user.id)
+      find('input.ra-multiselect-search').set('T')
+      page.execute_script("$('input.ra-multiselect-search').trigger('focus')")
+      page.execute_script("$('input.ra-multiselect-search').trigger('keydown')")
+      find('.ra-multiselect-collection option', text: teams[1].name).select_option
+      find('.ra-multiselect-item-add').click
+      click_button 'Save'
+      expect(ManagingUser.first.teams).to match_array teams
+    end
+  end
 end

lib/rails_admin/config/fields/association.rb
@@ -78,7 +78,7 @@ module RailsAdmin
 
         # Reader for associated primary key
         def associated_primary_key
-          @associated_primary_key ||= association.primary_key
+          @associated_primary_key ||= associated_model_config.abstract_model.primary_key
         end
 
         # Reader for the association's key

spec/controllers/rails_admin/main_controller_spec.rb
@@ -204,14 +204,15 @@ describe RailsAdmin::MainController, type: :controller do
   end
 
   describe 'index' do
-    it "uses source association's primary key with :compact, not target model's default primary key", skip_mongoid: true do
+    it "uses target model's primary key", skip_mongoid: true do
+      @player = FactoryBot.create(:player, number: 123)
       class TeamWithNumberedPlayers < Team
         has_many :numbered_players, class_name: 'Player', primary_key: :number, foreign_key: 'team_id'
       end
       FactoryBot.create :team
-      TeamWithNumberedPlayers.first.numbered_players = [FactoryBot.create(:player, number: 123)]
+      TeamWithNumberedPlayers.first.numbered_players = [@player]
       get :index, model_name: 'player', source_object_id: Team.first.id, source_abstract_model: 'team_with_numbered_players', associated_collection: 'numbered_players', current_action: :create, compact: true, format: :json
-      expect(response.body).to match(/\"id\":\"123\"/)
+      expect(response.body).to match(/\"id\":\"#{@player.id}\"/)
     end
 
     context 'as JSON' do

commit 0e92ca43fe7782a8d62ae9285a8d3de7857c9853

Fix be7d2f4a broke belongs_to with custom primary key
Fixes #3184

lib/rails_admin/adapters/mongoid/association.rb
@@ -41,7 +41,7 @@ module RailsAdmin
         end
 
         def primary_key
-          :_id
+          association.primary_key.to_sym rescue :_id
         end
 
         def foreign_key

lib/rails_admin/config/actions/index.rb
@@ -49,7 +49,7 @@ module RailsAdmin
               format.json do
                 output = begin
                   if params[:compact]
-                    primary_key_method = @model_config.abstract_model.primary_key
+                    primary_key_method = @association ? @association.associated_primary_key : @model_config.abstract_model.primary_key
                     label_method = @model_config.object_label_method
                     @objects.collect { |o| {id: o.send(primary_key_method).to_s, label: o.send(label_method).to_s} }
                   else

lib/rails_admin/config/fields/types/belongs_to_association.rb
@@ -35,6 +35,10 @@ module RailsAdmin
             true
           end
 
+          def associated_primary_key
+            @associated_primary_key ||= association.primary_key
+          end
+
           def selected_id
             bindings[:object].send(foreign_key)
           end

spec/dummy_app/app/active_record/managing_user.rb
@@ -1,3 +1,8 @@
 class ManagingUser < User
+  has_one :team, class_name: 'ManagedTeam', foreign_key: :manager, primary_key: :email, inverse_of: :user
   has_many :teams, class_name: 'ManagedTeam', foreign_key: :manager, primary_key: :email, inverse_of: :user
+
+  def team_id=(id)
+    self.team = ManagedTeam.find_by_id(id)
+  end
 end

spec/dummy_app/app/mongoid/managing_user.rb
@@ -1,3 +1,8 @@
 class ManagingUser < User
+  has_one :team, class_name: 'ManagedTeam', foreign_key: :manager, primary_key: :email, inverse_of: :user
   has_many :teams, class_name: 'ManagedTeam', foreign_key: :manager, primary_key: :email, inverse_of: :user
+
+  def team_id=(id)
+    self.team = ManagedTeam.where(_id: id).first
+  end
 end

spec/integration/fields/belongs_to_association_spec.rb
@@ -42,18 +42,42 @@ RSpec.describe 'BelongsToAssociation field', type: :request do
   end
 
   context 'with custom primary_key option' do
-    let(:user) { FactoryBot.create :managing_user }
-    let!(:teams) { [FactoryBot.create(:managed_team, manager: user.email), FactoryBot.create(:managed_team)] }
+    let(:users) { FactoryBot.create_list :managing_user, 2 }
+    let!(:teams) { [FactoryBot.create(:managed_team, manager: users[0].email), FactoryBot.create(:managed_team)] }
     before do
-      RailsAdmin.config.included_models = [ManagedTeam]
+      RailsAdmin.config.included_models = [ManagedTeam, ManagingUser]
       RailsAdmin.config ManagedTeam do
         field :user
       end
     end
 
-    it 'picks up associated primary key correctly' do
+    it 'allows update' do
       visit edit_path(model_name: 'managed_team', id: teams[0].id)
-      expect(page).to have_css("select#managed_team_manager option[value=\"#{user.id}\"]")
+      expect(page).to have_css("select#managed_team_manager option[value=\"#{users[0].email}\"]")
+      select("ManagingUser ##{users[1].id}", from: 'User')
+      click_button 'Save'
+      teams[0].reload
+      expect(teams[0].user).to eq users[1]
+    end
+
+    context 'when fetching associated objects via xhr' do
+      before do
+        RailsAdmin.config ManagedTeam do
+          field(:user) { associated_collection_cache_all false }
+        end
+      end
+
+      it 'allows update', js: true do
+        visit edit_path(model_name: 'managed_team', id: teams[0].id)
+        find('input.ra-filtering-select-input').set('M')
+        page.execute_script("$('input.ra-filtering-select-input').trigger('focus')")
+        page.execute_script("$('input.ra-filtering-select-input').trigger('keydown')")
+        expect(page).to have_selector('ul.ui-autocomplete li.ui-menu-item a')
+        page.execute_script %{$('ul.ui-autocomplete li.ui-menu-item a:contains("ManagingUser ##{users[1].id}")').trigger('mouseenter').click()}
+        click_button 'Save'
+        teams[0].reload
+        expect(teams[0].user).to eq users[1]
+      end
     end
   end
 end

spec/integration/fields/has_many_association_spec.rb
@@ -255,24 +255,35 @@ RSpec.describe 'HasManyAssociation field', type: :request do
     before do
       RailsAdmin.config.included_models = [ManagingUser, ManagedTeam]
       RailsAdmin.config ManagingUser do
-        field(:teams) { associated_collection_cache_all false }
+        field :teams
       end
     end
 
-    it "picks up associated model's primary key correctly" do
+    it "allows update" do
       visit edit_path(model_name: 'managing_user', id: user.id)
       expect(find("select#managing_user_team_ids option[value=\"#{teams[0].id}\"]")).to have_content teams[0].name
-    end
-
-    it "allows update with fetching associated objects via xhr", js: true do
-      visit edit_path(model_name: 'managing_user', id: user.id)
-      find('input.ra-multiselect-search').set('T')
-      page.execute_script("$('input.ra-multiselect-search').trigger('focus')")
-      page.execute_script("$('input.ra-multiselect-search').trigger('keydown')")
-      find('.ra-multiselect-collection option', text: teams[1].name).select_option
-      find('.ra-multiselect-item-add').click
+      select(teams[1].name, from: 'Teams')
       click_button 'Save'
       expect(ManagingUser.first.teams).to match_array teams
+    end
+
+    context 'when fetching associated objects via xhr' do
+      before do
+        RailsAdmin.config ManagingUser do
+          field(:teams) { associated_collection_cache_all false }
+        end
+      end
+
+      it "allows update", js: true do
+        visit edit_path(model_name: 'managing_user', id: user.id)
+        find('input.ra-multiselect-search').set('T')
+        page.execute_script("$('input.ra-multiselect-search').trigger('focus')")
+        page.execute_script("$('input.ra-multiselect-search').trigger('keydown')")
+        find('.ra-multiselect-collection option', text: teams[1].name).select_option
+        find('.ra-multiselect-item-add').click
+        click_button 'Save'
+        expect(ManagingUser.first.teams).to match_array teams
+      end
     end
   end
 end

spec/integration/fields/has_one_assosiation_spec.rb
@@ -92,4 +92,41 @@ RSpec.describe 'HasOneAssociation field', type: :request do
       expect(@record.comment).to be_nil
     end
   end
+
+  context 'with custom primary_key option' do
+    let(:user) { FactoryBot.create :managing_user }
+    let!(:team) { FactoryBot.create(:managed_team) }
+    before do
+      RailsAdmin.config.included_models = [ManagingUser, ManagedTeam]
+      RailsAdmin.config ManagingUser do
+        field :team
+      end
+    end
+
+    it "allows update" do
+      visit edit_path(model_name: 'managing_user', id: user.id)
+      select(team.name, from: 'Team')
+      click_button 'Save'
+      expect(ManagingUser.first.team).to eq team
+    end
+
+    context 'when fetching associated objects via xhr' do
+      before do
+        RailsAdmin.config ManagingUser do
+          field(:team) { associated_collection_cache_all false }
+        end
+      end
+
+      it "allows update", js: true do
+        visit edit_path(model_name: 'managing_user', id: user.id)
+        find('input.ra-filtering-select-input').set('T')
+        page.execute_script("$('input.ra-filtering-select-input').trigger('focus')")
+        page.execute_script("$('input.ra-filtering-select-input').trigger('keydown')")
+        expect(page).to have_selector('ul.ui-autocomplete li.ui-menu-item a')
+        page.execute_script %{$('ul.ui-autocomplete li.ui-menu-item a:contains("#{team.name}")').trigger('mouseenter').click()}
+        click_button 'Save'
+        expect(ManagingUser.first.team).to eq team
+      end
+    end
+  end
 end