3e71c7f1988675708618d33d9b276ada55aeae7f
tom-lord <lord.thom@gmail.com>
2020-01-02 17:29:50 +0000

    Add specs

--- spec/rails_admin/support/datetime_spec.rb
+++ spec/rails_admin/support/datetime_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe RailsAdmin::Support::Datetime do
       end
     end
   end
+
+  describe '.delocalize' do
+    around do |example|
+      original_locale = I18n.locale
+      I18n.locale = test_locale
+      example.run
+      I18n.locale = original_locale
+    end
+    let(:format) { '%a %A %d %b %B %I:%M%p' }
+    let(:english_translation) { 'Thu Thursday 02 Jan January 05:30pm' }
+
+    context 'when en locale' do
+      let(:test_locale) { :en }
+      let(:input) { english_translation }
+
+      it 'returns the original string' do
+        result = RailsAdmin::Support::Datetime.delocalize(input, format)
+        expect(result).to eq(input)
+      end
+    end
+
+    context 'when non-en locale' do
+      let(:test_locale) { :fr }
+      # Note: By default, rails-i18n uses all lower-case for Spanish day/month names
+      let(:input) { 'Jeu Jeudi 02 Jan Janvier 05:30pm' }
+
+      it 'returns the English translated string' do
+        result = RailsAdmin::Support::Datetime.delocalize(input, format)
+        expect(result).to eq(english_translation)
+      end
+    end
+  end
 end
