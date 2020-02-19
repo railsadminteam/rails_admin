d3ea5995eadaa360b293ef2ef3909f23a90357c9
kallelundgren93 <kalle@saits.se>
2020-02-19 16:58:10 +0100

    added support for new rails 6 default image_processing for active_storage_variants

--- lib/rails_admin/config/fields/types/multiple_active_storage.rb
+++ lib/rails_admin/config/fields/types/multiple_active_storage.rb
@@ -9,7 +9,11 @@ module RailsAdmin
 
           class ActiveStorageAttachment < RailsAdmin::Config::Fields::Types::MultipleFileUpload::AbstractAttachment
             register_instance_option :thumb_method do
-              {resize: '100x100>'}
+              if Gem.loaded_specs.key?('ruby-vips')
+                {resize_to_limit: [100, 100]}
+              else
+                {resize: '100x100>'}
+              end
             end
 
             register_instance_option :delete_value do
