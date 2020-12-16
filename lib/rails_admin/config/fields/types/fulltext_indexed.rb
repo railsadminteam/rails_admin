17b5f0a28a635b8061df7162d91607eea6ef8fac
Mitch Kett <mitch.kett@zendesk.com>
2020-12-16 14:46:34 -0500

    Refactor duplicate code, inherit from text field instead

--- lib/rails_admin/config/fields/types/fulltext_indexed.rb
+++ lib/rails_admin/config/fields/types/fulltext_indexed.rb
@@ -4,20 +4,8 @@ module RailsAdmin
   module Config
     module Fields
       module Types
-        class FulltextIndexed < StringLike
+        class FulltextIndexed < Text
           RailsAdmin::Config::Fields::Types.register(self)
-
-          register_instance_option :html_attributes do
-            {
-              required: required?,
-              cols: '48',
-              rows: '3',
-            }
-          end
-
-          register_instance_option :partial do
-            :form_text
-          end
         end
       end
     end
