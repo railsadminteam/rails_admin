b7c4d5124f44d516e3d3a7388a6a185740c0358b
Alex Schmitt <alex@alchemist.codes>
2021-08-09 14:51:05 -0700

    Remove unnecessary PaperTrail V10 version checks

--- spec/dummy_app/app/active_record/paper_trail_test_with_custom_association.rb
+++ spec/dummy_app/app/active_record/paper_trail_test_with_custom_association.rb
@@ -1,8 +1,4 @@
 class PaperTrailTestWithCustomAssociation < ActiveRecord::Base
   self.table_name = :paper_trail_tests
-  if PaperTrail::VERSION::MAJOR >= 10
-    has_paper_trail versions: {name: :trails}
-  else
-    has_paper_trail versions: :trails
-  end
+  has_paper_trail versions: {name: :trails}
 end
