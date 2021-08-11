6a1a1262853e6e975558d9908f071118727a01c4
Alex Schmitt <alex@alchemist.codes>
2021-08-11 08:50:02 -0700

    Simplify configs for numeric field types
    Addresses issues raised by CodeClimate in https://codeclimate.com/github/sferik/rails_admin/pull/3378 and further consolidates a shared base field config for numeric types

--- lib/rails_admin/config/fields/types/decimal.rb
+++ lib/rails_admin/config/fields/types/decimal.rb
@@ -1,16 +1,12 @@
-require 'rails_admin/config/fields/base'
+require 'rails_admin/config/fields/types/numeric'
 
 module RailsAdmin
   module Config
     module Fields
       module Types
-        class Decimal < RailsAdmin::Config::Fields::Base
+        class Decimal < RailsAdmin::Config::Fields::Types::Numeric
           # Register field type for the type loader
           RailsAdmin::Config::Fields::Types.register(self)
-
-          register_instance_option :view_helper do
-            :number_field
-          end
 
           register_instance_option :html_attributes do
             {
--- lib/rails_admin/config/fields/types/numeric.rb
+++ lib/rails_admin/config/fields/types/numeric.rb
@@ -1,15 +1,15 @@
-require 'rails_admin/config/fields/types/numeric'
+require 'rails_admin/config/fields/base'
 
 module RailsAdmin
   module Config
     module Fields
       module Types
-        class Integer < RailsAdmin::Config::Fields::Types::Numeric
+        class Numeric < RailsAdmin::Config::Fields::Base
           # Register field type for the type loader
           RailsAdmin::Config::Fields::Types.register(self)
 
-          register_instance_option :sort_reverse? do
-            serial?
+          register_instance_option :view_helper do
+            :number_field
           end
         end
       end
