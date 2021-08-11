01ab53087ed2c1ec779aa6ffff99bf8f04ef3ddf
Alex Schmitt <alex@alchemist.codes>
2021-08-11 08:05:31 -0700

    Add HTML5 validation for `float` fields
    These are currently rendered as text inputs (the default).
    
    ref: https://www.w3.org/TR/2012/WD-html5-20121025/common-input-element-attributes.html#attr-input-step

--- lib/rails_admin/config/fields/types/float.rb
+++ lib/rails_admin/config/fields/types/float.rb
@@ -7,6 +7,17 @@ module RailsAdmin
         class Float < RailsAdmin::Config::Fields::Base
           # Register field type for the type loader
           RailsAdmin::Config::Fields::Types.register(self)
+
+          register_instance_option :view_helper do
+            :number_field
+          end
+
+          register_instance_option :html_attributes do
+            {
+              required: required?,
+              step: "any",
+            }
+          end
         end
       end
     end
--- spec/rails_admin/config/fields/types/float_spec.rb
+++ spec/rails_admin/config/fields/types/float_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe RailsAdmin::Config::Fields::Types::Float do
+  subject do
+    RailsAdmin.config('FieldTest').fields.detect do |f|
+      f.name == :float_field
+    end.with(object: FieldTest.new)
+  end
+
+  describe '#html_attributes' do
+    it 'should contain a step attribute' do
+      expect(subject.html_attributes[:step]).to eq('any')
+    end
+  end
+
+  describe '#view_helper' do
+    it "uses the 'number' type input tag" do
+      expect(subject.view_helper).to eq(:number_field)
+    end
+  end
+
   it_behaves_like 'a generic field type', :float_field, :float
 end
