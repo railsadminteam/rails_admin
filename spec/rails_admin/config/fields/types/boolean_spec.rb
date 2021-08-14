01b8b77d1d4de057014e79a961a14fb73b62ed06
Alex Schmitt <alex@alchemist.codes>
2021-08-14 07:45:29 -0700

    Add spec for Boolean field #pretty_value

--- spec/rails_admin/config/fields/types/boolean_spec.rb
+++ spec/rails_admin/config/fields/types/boolean_spec.rb
@@ -2,4 +2,27 @@ require 'spec_helper'
 
 RSpec.describe RailsAdmin::Config::Fields::Types::Boolean do
   it_behaves_like 'a generic field type', :boolean_field, :boolean
+
+  subject do
+    RailsAdmin.config(FieldTest).fields.detect do |f|
+      f.name == :boolean_field
+    end.with(object: test_object)
+  end
+
+  describe '#pretty_value' do
+    {
+      false => %(<span class='label label-danger'>&#x2718;</span>),
+      true => %(<span class='label label-success'>&#x2713;</span>),
+      nil => %(<span class='label label-default'>&#x2012;</span>),
+    }.each do |field_value, expected_result|
+
+      context "when field value is '#{field_value.inspect}'" do
+        let(:test_object) { FieldTest.new(boolean_field: field_value) }
+
+        it "returns the appropriate html result" do
+          expect(subject.pretty_value).to eq(expected_result)
+        end
+      end
+    end
+  end
 end
