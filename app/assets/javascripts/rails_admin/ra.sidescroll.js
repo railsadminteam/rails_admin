e6604646703fec0f23b1c373ec803540cad05959
Jon Dufresne <jon.dufresne@gmail.com>
2021-11-14 08:11:31 -0800

    Remove nearly all jQuery from ra.sidescroll.js
    All DOM methods now use native browser features.
    
    The only remaining jQuery is used to listen to the custom jQuery event
    "rails_admin.dom_ready".
    
    Refs #2893

--- app/assets/javascripts/rails_admin/ra.sidescroll.js
+++ app/assets/javascripts/rails_admin/ra.sidescroll.js
@@ -1,34 +1,27 @@
 (function ($) {
-  function setFrozenColPositions() {
-    var $listForm, frozenColumns;
+  "use strict";
 
-    $listForm = $("#bulk_form");
-    if (!$listForm.is(".ra-sidescroll")) {
+  $(document).on("rails_admin.dom_ready", () => {
+    const listForm = document.getElementById("bulk_form");
+    if (!listForm || !listForm.classList.contains("ra-sidescroll")) {
       return;
     }
-    frozenColumns = $listForm.data("ra-sidescroll");
 
-    $listForm.find("table tr").each(function (index, tr) {
-      var firstPosition = 0;
-
-      $(tr)
-        .children()
+    const frozenColumns = Number(listForm.dataset.raSidescroll);
+    listForm.querySelectorAll("tr").forEach((tr, index) => {
+      let firstPosition;
+      Array.from(tr.children)
         .slice(0, frozenColumns)
-        .each(function (idx, td) {
-          var tdLeft;
-          $(td).addClass("ra-sidescroll-frozen");
+        .forEach((td, idx) => {
+          td.classList.add("ra-sidescroll-frozen");
           if (idx === frozenColumns - 1) {
-            $(td).addClass("last-of-frozen");
+            td.classList.add("last-of-frozen");
           }
-          tdLeft = $(td).position().left;
           if (idx === 0) {
-            firstPosition = tdLeft;
+            firstPosition = td.offsetLeft;
           }
-          td.style.left = tdLeft - firstPosition + "px";
+          td.style.left = `${td.offsetLeft - firstPosition}px`;
         });
     });
-  }
-
-  $(window).on("load", setFrozenColPositions);
-  $(document).on("rails_admin.dom_ready", setFrozenColPositions);
+  });
 })(jQuery);
