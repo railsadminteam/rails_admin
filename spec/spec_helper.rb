71e02bcf71caf7a71bde6664e82ad48d4dca67c3
M.Shibuya <mit.shibuya@gmail.com>
2021-12-18 16:45:29 +0900

    Mitigate JRuby CI failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -90,7 +90,7 @@ RSpec.configure do |config|
   config.verbose_retry = true
   config.display_try_failure_messages = true
   config.around :each, :js do |example|
-    example.run_with_retry retry: 2
+    example.run_with_retry retry: (ENV['CI'] && RUBY_ENGINE == 'jruby' ? 3 : 2)
   end
   config.retry_callback = proc do |example|
     Capybara.reset! if example.metadata[:js]
