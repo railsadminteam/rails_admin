16e72b5dc09aec76a4c20eb1096a63a7bb3b4f5d
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2022-01-08 17:59:37 +0900

    Apply form-control-sm to make datepicker field have the same size with other inputs
    Co-authored-by: Alex Schmitt <codealchemy@users.noreply.github.com>

--- src/rails_admin/filter-box.js
+++ src/rails_admin/filter-box.js
@@ -110,7 +110,7 @@ import flatpickr from "flatpickr";
                 .addClass(index == 0 ? "default" : "between")
                 .css("display", visible ? "inline-block" : "none")
                 .html(
-                  $('<input class="input-sm form-control" type="text" />')
+                  $('<input class="input-sm form-control-sm" type="text" />')
                     .addClass(field_type == "date" ? "date" : "datetime")
                     .prop("name", value_name + "[]")
                     .prop("value", field_value[index] || "")
