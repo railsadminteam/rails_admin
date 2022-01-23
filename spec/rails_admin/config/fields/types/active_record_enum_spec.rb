82cad9e00accf9551ccf446646610e30e5212be8
M.Shibuya <mit.shibuya@gmail.com>
2022-01-23 22:43:14 +0900

    Ensure 'format' can be used as an ActiveRecord enum name
    Closes #3324

--- spec/rails_admin/config/fields/types/active_record_enum_spec.rb
+++ spec/rails_admin/config/fields/types/active_record_enum_spec.rb
@@ -2,4 +2,23 @@ require 'spec_helper'
 
 RSpec.describe RailsAdmin::Config::Fields::Types::ActiveRecordEnum, active_record: true do
   it_behaves_like 'a generic field type', :string_enum_field
+
+  describe '#pretty_value' do
+    context 'when column name is format' do
+      before do
+        class FormatAsEnum < FieldTest
+          enum format: {Text: 'txt', Markdown: 'md'}
+        end
+      end
+      let(:field) do
+        RailsAdmin.config(FormatAsEnum).fields.detect do |f|
+          f.name == :format
+        end.with(object: FormatAsEnum.new(format: 'md'))
+      end
+
+      it 'does not break' do
+        expect(field.pretty_value).to eq 'Markdown'
+      end
+    end
+  end
 end
