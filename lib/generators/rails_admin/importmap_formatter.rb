d04c8310804370b322aa4dc27f517703bc572c68
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2022-03-04 18:07:53 +0900

    Work around ReDoS vulnerability

--- lib/generators/rails_admin/importmap_formatter.rb
+++ lib/generators/rails_admin/importmap_formatter.rb
@@ -18,7 +18,7 @@ module RailsAdmin
 
       # Tidy up jQuery UI dependencies
       jquery_uis = imports.keys.filter { |key, _| key =~ /jquery-ui/ }
-      imports['jquery-ui/'] = imports[jquery_uis.first].gsub(%r{(/[^@]+)+$}, '/')
+      imports['jquery-ui/'] = imports[jquery_uis.first].gsub(%r{(@[^/@]+)/[^@]+$}, '\1/')
       imports.reject! { |key, _| jquery_uis.include? key }
 
       pins = ['pin "rails_admin", preload: true', packager.pin_for('rails_admin/src/rails_admin/base', imports.delete('rails_admin'))]
