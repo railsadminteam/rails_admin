5b1294842776b59c6e03463f8d1f18922f04cbef
Ferdi van der Werf <ferdi@rootnet.nl>
2022-04-07 16:06:11 +0200

    Remove superfluous space from root_navigation

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -94,7 +94,7 @@ module RailsAdmin
       actions(:root).select(&:show_in_sidebar).group_by(&:sidebar_label).collect do |label, nodes|
         li_stack = nodes.map do |node|
           url = rails_admin.url_for(action: node.action_name, controller: 'rails_admin/main')
-          nav_icon = node.link_icon ? %(<i class="#{node.link_icon}"></i> ).html_safe : ''
+          nav_icon = node.link_icon ? %(<i class="#{node.link_icon}"></i>).html_safe : ''
           content_tag :li do
             link_to nav_icon + " " + wording_for(:menu, node), url, class: "nav-link"
           end
