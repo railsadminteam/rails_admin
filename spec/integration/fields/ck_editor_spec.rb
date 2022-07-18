ec83444d335eea93190b70401399a3a1bec25bb6
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2022-07-18 18:05:16 +0900

    Add safeguard fot the spec failure
    https://github.com/railsadminteam/rails_admin/runs/7385621766?check_suite_focus=true

--- spec/integration/fields/ck_editor_spec.rb
+++ spec/integration/fields/ck_editor_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe 'CKEditor field', type: :request do
     visit new_path(model_name: 'player')
     find_link('Add a new Comment').trigger 'click'
     expect(page).to have_text 'New Comment'
+    expect(page).to have_css '.cke_button__link'
     find('.cke_button__link').click
     expect(page).to have_css '.cke_dialog_ui_input_text'
     first(:css, '.cke_dialog_ui_input_text').click
