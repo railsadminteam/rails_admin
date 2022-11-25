de2b36116b1a7c5087cecd70a430d0ae17dc4e0c
Waymond Chen <wayc@users.noreply.github.com>
2022-11-25 16:22:41 +0000

    Remove unary operator

--- lib/rails_admin/config/configurable.rb
+++ lib/rails_admin/config/configurable.rb
@@ -57,7 +57,7 @@ module RailsAdmin
 
           # If it's a boolean create an alias for it and remove question mark
           if option_name.end_with?('?')
-            scope.send(:define_method, "#{(+option_name).chop!}?") do
+            scope.send(:define_method, "#{option_name.chop!}?") do
               send(option_name)
             end
           end
