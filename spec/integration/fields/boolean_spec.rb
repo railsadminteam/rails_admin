321b3dea74ea5a40f5de296c5230cb0905625fad
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-03-28 12:52:07 +0200

    Add Spec Test for boolean if not nulable

--- spec/integration/fields/boolean_spec.rb
+++ spec/integration/fields/boolean_spec.rb
@@ -53,19 +53,27 @@ RSpec.describe 'Boolean field', type: :request do
       end
     end
 
-    it 'shows a checkbox' do
+    it 'shows 2 radio buttons' do
       visit new_path(model_name: 'field_test')
       is_expected.to have_content 'New Field test'
-      is_expected.to have_css '[type="checkbox"][name="field_test[boolean_field]"]'
+      expect(all('[name="field_test[boolean_field]"]').map { |e| e['value'] }).to eq ['1', '0']
     end
 
     it 'can be updated' do
       visit edit_path(model_name: 'field_test', id: field_test.id)
-      find('.boolean_type input').check
+
+      # change the value to true and assert the values
+      find('.boolean_type label.success').click
       click_button 'Save and edit'
+      # validate that the success button rendered and is active
+      expect(page).to have_selector('.boolean_type input[value="1"][checked]')
+      # validate the value is true
       expect(field_test.reload.boolean_field).to be true
-      find('.boolean_type input').uncheck
+
+      # change the value to false and assert the values
+      find('.boolean_type label.danger').click
       click_button 'Save and edit'
+      expect(page).to have_selector('.boolean_type input[value="0"][checked]')
       expect(field_test.reload.boolean_field).to be false
     end
   end
