afee74e4fb7cb0b2ec0168475ae6ee606cdf02c5
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2024-07-14 14:47:29 +0900

    Remove unused code in Dragonfly field

--- spec/rails_admin/config/fields/types/drangonfly_spec.rb
+++ spec/rails_admin/config/fields/types/drangonfly_spec.rb
@@ -5,6 +5,29 @@ require 'spec_helper'
 RSpec.describe RailsAdmin::Config::Fields::Types::Dragonfly do
   it_behaves_like 'a generic field type', :string_field, :dragonfly
 
+  let(:field) do
+    RailsAdmin.config('FieldTest').fields.detect do |f|
+      f.name == :dragonfly_asset
+    end.with(object: record)
+  end
+
+  describe '#image?' do
+    let(:file) { File.open(file_path('test.jpg')) }
+    let(:record) { FactoryBot.create :field_test, dragonfly_asset: file }
+
+    it 'returns true' do
+      expect(field.image?).to be true
+    end
+
+    context 'with non-image' do
+      let(:file) { File.open(file_path('test.txt')) }
+
+      it 'returns false' do
+        expect(field.image?).to be false
+      end
+    end
+  end
+
   describe 'with a model which does not extend Dragonfly::Model' do
     before do
       class NonDragonflyTest < Tableless
