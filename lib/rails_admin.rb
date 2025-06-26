e044d715b34fd9f47e3ba8dcbe12562cb6011759
Weiyuan Liu <weiyuan.lane@gmail.com>
2025-06-26 12:20:00 +0800

    [Ops] Test fix for rails_admin

--- lib/rails_admin.rb
+++ lib/rails_admin.rb
@@ -49,18 +49,34 @@ module RailsAdmin
   rescue LoadError
     if YAML.respond_to?(:safe_load)
       def self.yaml_load(yaml)
-        # Note that this only works for Rails 6
-        #   - Ref: https://github.com/rails/rails/blob/v6.1.7.6/activerecord/lib/active_record/coders/yaml_column.rb#L50-L56
-        if ActiveRecord::Base.respond_to?(:use_yaml_unsafe_load) && ActiveRecord::Base.use_yaml_unsafe_load
-          YAML.unsafe_load(yaml)
-        # Note that this only works for Rails 7
-        #   - Ref: https://github.com/rails/rails/blob/v7.1.4.2/activerecord/lib/active_record/coders/yaml_column.rb#L16
-        elsif ActiveRecord.respond_to?(:use_yaml_unsafe_load) && ActiveRecord.use_yaml_unsafe_load
+        use_yaml_unsafe_load = if ActiveRecord::Base.respond_to?(:use_yaml_unsafe_load)
+          # Note that this only works for Rails 6
+          #   - Ref: https://github.com/rails/rails/blob/v6.1.7.6/activerecord/lib/active_record/coders/yaml_column.rb#L50-L56
+          ActiveRecord::Base.use_yaml_unsafe_load
+        elsif ActiveRecord.respond_to?(:use_yaml_unsafe_load)
+          # Note that this only works for Rails 7
+          #   - Ref: https://github.com/rails/rails/blob/v7.1.4.2/activerecord/lib/active_record/coders/yaml_column.rb#L16
+          ActiveRecord.use_yaml_unsafe_load
+        else
+          raise LoadError.new "Unknown attributes needed for safe-loading of YAML. Please verify for the ActiveRecord config of `use_yaml_unsafe_load` and plug your implementation in here."
+        end
+
+        yaml_column_permitted_classes = if ActiveRecord::Base.respond_to?(:yaml_column_permitted_classes)
+          # Note that this only works for Rails 6
+          ActiveRecord::Base.yaml_column_permitted_classes
+        elsif ActiveRecord.respond_to?(:yaml_column_permitted_classes)
+          # Note that this only works for Rails 7
+          ActiveRecord.yaml_column_permitted_classes
+        else
+          raise LoadError.new "Unknown attributes needed for safe-loading of YAML. Please verify for the ActiveRecord config of `yaml_column_permitted_classes` and plug your implementation in here."
+        end
+
+        if use_yaml_unsafe_load
           YAML.unsafe_load(yaml)
         elsif YAML.method(:safe_load).parameters.include?([:key, :permitted_classes])
-          YAML.safe_load(yaml, permitted_classes: ActiveRecord::Base.yaml_column_permitted_classes, aliases: true)
+          YAML.safe_load(yaml, permitted_classes: yaml_column_permitted_classes, aliases: true)
         else
-          YAML.safe_load(yaml, ActiveRecord::Base.yaml_column_permitted_classes, [], true)
+          YAML.safe_load(yaml, yaml_column_permitted_classes, [], true)
         end
       end
     else
